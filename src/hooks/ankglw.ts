6c3ced7b0c444be6798b02a297869df20cab1fcf
akabariprince <princeakabari123@gmail.com>
2018-03-26 17:02:00 +0530

    fix: bug in dashboard chart rendering

--- src/hooks/ankglw.ts
+++ src/hooks/ankglw.ts
@@ -5,3 +5,4 @@ export const ankglw = () => {
 // auto-update 2025-09-08T02:30:45.238Z
 // auto-update 2025-09-08T02:30:46.528Z
 // auto-update 2025-09-08T02:30:52.281Z
+// auto-update 2025-09-08T02:30:53.633Z
