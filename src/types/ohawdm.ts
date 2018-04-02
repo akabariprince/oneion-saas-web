51f118b348ee75ff2c40f73fa25a33c65bdfc504
akabariprince <princeakabari123@gmail.com>
2018-04-02 18:49:00 +0530

    fix: responsive design issue

--- src/types/ohawdm.ts
+++ src/types/ohawdm.ts
@@ -6,3 +6,4 @@ export const ohawdm = () => {
 // auto-update 2025-09-08T02:30:45.078Z
 // auto-update 2025-09-08T02:30:47.610Z
 // auto-update 2025-09-08T02:30:48.000Z
+// auto-update 2025-09-08T02:30:54.267Z
