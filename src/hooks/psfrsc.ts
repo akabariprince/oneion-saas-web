b432c65754939de09274cb84dba483f4e1a7118a
akabariprince <princeakabari123@gmail.com>
2018-05-16 19:06:00 +0530

    test: add unit tests for auth

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -7,3 +7,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:30:52.640Z
 // auto-update 2025-09-08T02:30:56.229Z
 // auto-update 2025-09-08T02:30:58.169Z
+// auto-update 2025-09-08T02:30:59.044Z
