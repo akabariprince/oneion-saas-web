e2a7ad1a9a9e260db6ff5a8dd68e331cd1ae3428
akabariprince <princeakabari123@gmail.com>
2018-06-06 18:46:00 +0530

    fix: bug in dashboard chart rendering

--- src/utils/gbftyp.ts
+++ src/utils/gbftyp.ts
@@ -5,3 +5,4 @@ export const gbftyp = () => {
 // auto-update 2025-09-08T02:30:46.608Z
 // auto-update 2025-09-08T02:30:52.600Z
 // auto-update 2025-09-08T02:30:58.130Z
+// auto-update 2025-09-08T02:31:01.223Z
