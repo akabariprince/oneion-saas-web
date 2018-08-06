44109f120de5e15d0897c78add759a4cfe5e508c
akabariprince <princeakabari123@gmail.com>
2018-08-06 16:02:00 +0530

    fix: handle null pointer in API

--- src/hooks/ufoquj.ts
+++ src/hooks/ufoquj.ts
@@ -5,3 +5,4 @@ export const ufoquj = () => {
 // auto-update 2025-09-08T02:30:47.690Z
 // auto-update 2025-09-08T02:30:56.469Z
 // auto-update 2025-09-08T02:30:56.932Z
+// auto-update 2025-09-08T02:31:08.957Z
