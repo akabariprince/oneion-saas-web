acc919355876b9e65cf2356b3cf2701affeab222
akabariprince <princeakabari123@gmail.com>
2018-08-17 11:01:00 +0530

    fix: handle null pointer in API

--- src/services/tkdzmj.ts
+++ src/services/tkdzmj.ts
@@ -5,3 +5,4 @@ export const tkdzmj = () => {
 // auto-update 2025-09-08T02:30:52.880Z
 // auto-update 2025-09-08T02:30:56.891Z
 // auto-update 2025-09-08T02:30:58.010Z
+// auto-update 2025-09-08T02:31:10.494Z
