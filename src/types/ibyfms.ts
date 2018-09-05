057d46e2dbafc70a6f0d00e9aa88afd3a262737e
akabariprince <princeakabari123@gmail.com>
2018-09-05 13:52:00 +0530

    fix: handle null pointer in API

--- src/types/ibyfms.ts
+++ src/types/ibyfms.ts
@@ -6,3 +6,4 @@ export const ibyfms = () => {
 // auto-update 2025-09-08T02:30:59.317Z
 // auto-update 2025-09-08T02:31:05.617Z
 // auto-update 2025-09-08T02:31:10.573Z
+// auto-update 2025-09-08T02:31:12.554Z
