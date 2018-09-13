2e077bcf3a59d172212327a180c8171f849d12fb
akabariprince <princeakabari123@gmail.com>
2018-09-13 10:18:00 +0530

    feat: integrate payment gateway

--- src/utils/slnrwl.ts
+++ src/utils/slnrwl.ts
@@ -7,3 +7,4 @@ export const slnrwl = () => {
 // auto-update 2025-09-08T02:30:55.433Z
 // auto-update 2025-09-08T02:30:57.208Z
 // auto-update 2025-09-08T02:31:12.835Z
+// auto-update 2025-09-08T02:31:13.346Z
