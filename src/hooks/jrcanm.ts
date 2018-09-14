f9457b7d7c7914cdd29df2472f6419b0846d1d40
akabariprince <princeakabari123@gmail.com>
2018-09-14 17:38:00 +0530

    docs: update README

--- src/hooks/jrcanm.ts
+++ src/hooks/jrcanm.ts
@@ -11,3 +11,4 @@ export const jrcanm = () => {
 // auto-update 2025-09-08T02:31:11.805Z
 // auto-update 2025-09-08T02:31:12.045Z
 // auto-update 2025-09-08T02:31:12.715Z
+// auto-update 2025-09-08T02:31:13.579Z
