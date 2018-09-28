967af3ab8dffca82e5be1c14d1bae957c00554b9
akabariprince <princeakabari123@gmail.com>
2018-09-28 09:48:00 +0530

    fix: responsive design issue

--- src/services/ujapru.ts
+++ src/services/ujapru.ts
@@ -5,3 +5,4 @@ export const ujapru = () => {
 // auto-update 2025-09-08T02:30:49.564Z
 // auto-update 2025-09-08T02:30:56.113Z
 // auto-update 2025-09-08T02:31:09.349Z
+// auto-update 2025-09-08T02:31:15.240Z
