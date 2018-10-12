8e11f577761fb3688ca0a605e0142ce023098de6
akabariprince <princeakabari123@gmail.com>
2018-10-12 09:26:00 +0530

    docs: add API usage example

--- src/services/thepsc.ts
+++ src/services/thepsc.ts
@@ -7,3 +7,4 @@ export const thepsc = () => {
 // auto-update 2025-09-08T02:30:53.433Z
 // auto-update 2025-09-08T02:30:56.651Z
 // auto-update 2025-09-08T02:31:14.020Z
+// auto-update 2025-09-08T02:31:16.967Z
