28979a201c47e8f913f46cd7c998f76fd4aef1df
akabariprince <princeakabari123@gmail.com>
2018-10-23 13:56:00 +0530

    fix: handle null pointer in API

--- src/utils/xtoynk.ts
+++ src/utils/xtoynk.ts
@@ -6,3 +6,4 @@ export const xtoynk = () => {
 // auto-update 2025-09-08T02:30:48.767Z
 // auto-update 2025-09-08T02:30:52.126Z
 // auto-update 2025-09-08T02:30:53.513Z
+// auto-update 2025-09-08T02:31:18.270Z
