5404b885c85877edd9734b92549783f842921217
akabariprince <princeakabari123@gmail.com>
2018-10-25 12:31:00 +0530

    fix: handle null pointer in API

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -10,3 +10,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:30:59.044Z
 // auto-update 2025-09-08T02:31:06.882Z
 // auto-update 2025-09-08T02:31:09.428Z
+// auto-update 2025-09-08T02:31:18.467Z
