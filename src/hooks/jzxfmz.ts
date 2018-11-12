de380db4e3f46a0c7233685918dff1481be11565
akabariprince <princeakabari123@gmail.com>
2018-11-12 12:15:00 +0530

    fix: responsive design issue

--- src/hooks/jzxfmz.ts
+++ src/hooks/jzxfmz.ts
@@ -10,3 +10,4 @@ export const jzxfmz = () => {
 // auto-update 2025-09-08T02:31:08.600Z
 // auto-update 2025-09-08T02:31:10.065Z
 // auto-update 2025-09-08T02:31:12.083Z
+// auto-update 2025-09-08T02:31:19.696Z
