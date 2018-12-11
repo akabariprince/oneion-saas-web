32684647921dca6cf5145181dbc718394c10840f
akabariprince <princeakabari123@gmail.com>
2018-12-11 14:32:00 +0530

    fix: responsive design issue

--- src/pages/pjdmst.tsx
+++ src/pages/pjdmst.tsx
@@ -12,3 +12,4 @@ export default Pjdmst;
 // auto-update 2025-09-08T02:30:58.562Z
 // auto-update 2025-09-08T02:31:09.826Z
 // auto-update 2025-09-08T02:31:11.406Z
+// auto-update 2025-09-08T02:31:22.511Z
