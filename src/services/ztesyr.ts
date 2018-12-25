19c722ead484c132ac3258febc3186fc7d298a97
akabariprince <princeakabari123@gmail.com>
2018-12-25 14:52:00 +0530

    test: add unit tests for auth

--- src/services/ztesyr.ts
+++ src/services/ztesyr.ts
@@ -9,3 +9,4 @@ export const ztesyr = () => {
 // auto-update 2025-09-08T02:31:05.938Z
 // auto-update 2025-09-08T02:31:08.207Z
 // auto-update 2025-09-08T02:31:11.249Z
+// auto-update 2025-09-08T02:31:23.942Z
