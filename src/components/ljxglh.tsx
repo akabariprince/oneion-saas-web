00c46790b25c4a6ea62f0368d630bbd7e45d73d8
akabariprince <princeakabari123@gmail.com>
2018-12-26 13:14:00 +0530

    fix: responsive design issue

--- src/components/ljxglh.tsx
+++ src/components/ljxglh.tsx
@@ -12,3 +12,4 @@ export default Ljxglh;
 // auto-update 2025-09-08T02:30:52.165Z
 // auto-update 2025-09-08T02:30:55.119Z
 // auto-update 2025-09-08T02:31:21.121Z
+// auto-update 2025-09-08T02:31:24.061Z
