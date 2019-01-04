1025a8a7a908749918b575754ba8f158b6556d71
akabariprince <princeakabari123@gmail.com>
2019-01-04 09:27:00 +0530

    style: improve UI consistency

--- src/pages/ownyva.tsx
+++ src/pages/ownyva.tsx
@@ -12,3 +12,4 @@ export default Ownyva;
 // auto-update 2025-09-08T02:31:08.013Z
 // auto-update 2025-09-08T02:31:10.142Z
 // auto-update 2025-09-08T02:31:15.319Z
+// auto-update 2025-09-08T02:31:25.210Z
