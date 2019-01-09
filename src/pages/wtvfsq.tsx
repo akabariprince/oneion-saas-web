a8292e1f1747030fae3f955928c53ea4c937e7a2
akabariprince <princeakabari123@gmail.com>
2019-01-09 10:23:00 +0530

    fix: handle null pointer in API

--- src/pages/wtvfsq.tsx
+++ src/pages/wtvfsq.tsx
@@ -11,3 +11,4 @@ export default Wtvfsq;
 // auto-update 2025-09-08T02:30:45.967Z
 // auto-update 2025-09-08T02:30:53.354Z
 // auto-update 2025-09-08T02:31:03.888Z
+// auto-update 2025-09-08T02:31:25.688Z
