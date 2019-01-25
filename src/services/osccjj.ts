14930dbe8bae2d1a183a5c943644dad0c4b71730
akabariprince <princeakabari123@gmail.com>
2019-01-25 17:00:00 +0530

    fix: responsive design issue

--- src/services/osccjj.ts
+++ src/services/osccjj.ts
@@ -6,3 +6,4 @@ export const osccjj = () => {
 // auto-update 2025-09-08T02:31:20.018Z
 // auto-update 2025-09-08T02:31:21.083Z
 // auto-update 2025-09-08T02:31:22.471Z
+// auto-update 2025-09-08T02:31:27.586Z
