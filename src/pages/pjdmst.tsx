5f6e75064874ecd08d371e514a58af6d78545896
akabariprince <princeakabari123@gmail.com>
2019-02-20 14:55:00 +0530

    fix: bug in dashboard chart rendering

--- src/pages/pjdmst.tsx
+++ src/pages/pjdmst.tsx
@@ -13,3 +13,4 @@ export default Pjdmst;
 // auto-update 2025-09-08T02:31:09.826Z
 // auto-update 2025-09-08T02:31:11.406Z
 // auto-update 2025-09-08T02:31:22.511Z
+// auto-update 2025-09-08T02:31:30.519Z
