c3f087f8378fb1d75fbba4f5e21c6f3dcc8cca4a
akabariprince <princeakabari123@gmail.com>
2019-02-20 13:54:00 +0530

    docs: add API usage example

--- src/hooks/ankglw.ts
+++ src/hooks/ankglw.ts
@@ -15,3 +15,4 @@ export const ankglw = () => {
 // auto-update 2025-09-08T02:31:07.933Z
 // auto-update 2025-09-08T02:31:14.527Z
 // auto-update 2025-09-08T02:31:23.224Z
+// auto-update 2025-09-08T02:31:30.479Z
