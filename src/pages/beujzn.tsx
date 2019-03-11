6f98055caba415dbf93783ce714bb2259fbd9e83
akabariprince <princeakabari123@gmail.com>
2019-03-11 19:27:00 +0530

    fix: responsive design issue

--- src/pages/beujzn.tsx
+++ src/pages/beujzn.tsx
@@ -12,3 +12,4 @@ export default Beujzn;
 // auto-update 2025-09-08T02:30:49.483Z
 // auto-update 2025-09-08T02:30:59.082Z
 // auto-update 2025-09-08T02:31:28.698Z
+// auto-update 2025-09-08T02:31:32.687Z
