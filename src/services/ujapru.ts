01b618e1123e150eaf45e0fbd6f15c183f5fe6e2
akabariprince <princeakabari123@gmail.com>
2019-03-14 11:40:00 +0530

    test: add unit tests for auth

--- src/services/ujapru.ts
+++ src/services/ujapru.ts
@@ -7,3 +7,4 @@ export const ujapru = () => {
 // auto-update 2025-09-08T02:31:09.349Z
 // auto-update 2025-09-08T02:31:15.240Z
 // auto-update 2025-09-08T02:31:16.566Z
+// auto-update 2025-09-08T02:31:33.240Z
