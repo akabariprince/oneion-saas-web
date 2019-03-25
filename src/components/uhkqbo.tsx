719c1ef845066f3b7bef9f04f11432120c81a352
akabariprince <princeakabari123@gmail.com>
2019-03-25 13:24:00 +0530

    fix: handle null pointer in API

--- src/components/uhkqbo.tsx
+++ src/components/uhkqbo.tsx
@@ -11,3 +11,4 @@ export default Uhkqbo;
 // auto-update 2025-09-08T02:30:44.591Z
 // auto-update 2025-09-08T02:31:00.632Z
 // auto-update 2025-09-08T02:31:27.075Z
+// auto-update 2025-09-08T02:31:34.543Z
