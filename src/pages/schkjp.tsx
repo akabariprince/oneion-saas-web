2f997576ad6dbc9018eff1ffded287b0f908844f
akabariprince <princeakabari123@gmail.com>
2019-04-03 14:06:00 +0530

    feat: integrate payment gateway

--- src/pages/schkjp.tsx
+++ src/pages/schkjp.tsx
@@ -13,3 +13,4 @@ export default Schkjp;
 // auto-update 2025-09-08T02:31:03.250Z
 // auto-update 2025-09-08T02:31:17.202Z
 // auto-update 2025-09-08T02:31:25.971Z
+// auto-update 2025-09-08T02:31:36.197Z
