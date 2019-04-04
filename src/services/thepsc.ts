e43319f1aa1b5ef458ce00f3d425258b4d040df6
akabariprince <princeakabari123@gmail.com>
2019-04-04 16:40:00 +0530

    feat: add AI prediction API

--- src/services/thepsc.ts
+++ src/services/thepsc.ts
@@ -9,3 +9,4 @@ export const thepsc = () => {
 // auto-update 2025-09-08T02:31:14.020Z
 // auto-update 2025-09-08T02:31:16.967Z
 // auto-update 2025-09-08T02:31:36.080Z
+// auto-update 2025-09-08T02:31:36.393Z
