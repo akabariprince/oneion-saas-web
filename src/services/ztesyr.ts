c4972016cac02661b9ed052b411085e069ce3619
akabariprince <princeakabari123@gmail.com>
2019-04-18 19:07:00 +0530

    fix: responsive design issue

--- src/services/ztesyr.ts
+++ src/services/ztesyr.ts
@@ -11,3 +11,4 @@ export const ztesyr = () => {
 // auto-update 2025-09-08T02:31:11.249Z
 // auto-update 2025-09-08T02:31:23.942Z
 // auto-update 2025-09-08T02:31:28.260Z
+// auto-update 2025-09-08T02:31:37.940Z
