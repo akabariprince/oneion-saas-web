6ce6eb4abc92872062bb91f211be2d39f92e7435
akabariprince <princeakabari123@gmail.com>
2019-05-23 09:22:00 +0530

    fix: handle null pointer in API

--- src/utils/vudhzp.ts
+++ src/utils/vudhzp.ts
@@ -18,3 +18,4 @@ export const vudhzp = () => {
 // auto-update 2025-09-08T02:31:31.076Z
 // auto-update 2025-09-08T02:31:33.637Z
 // auto-update 2025-09-08T02:31:41.144Z
+// auto-update 2025-09-08T02:31:41.716Z
