ef475ed3696a28aeba1b7bc7b81810ab8575786d
akabariprince <princeakabari123@gmail.com>
2019-06-11 14:00:00 +0530

    chore: cleanup unused code

--- src/hooks/liefaf.ts
+++ src/hooks/liefaf.ts
@@ -15,3 +15,4 @@ export const liefaf = () => {
 // auto-update 2025-09-08T02:31:30.124Z
 // auto-update 2025-09-08T02:31:40.474Z
 // auto-update 2025-09-08T02:31:43.097Z
+// auto-update 2025-09-08T02:31:43.644Z
