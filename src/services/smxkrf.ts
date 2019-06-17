5c6ae0ae178eeea2d51b6c2599cd130af1236387
akabariprince <princeakabari123@gmail.com>
2019-06-17 16:31:00 +0530

    test: add unit tests for auth

--- src/services/smxkrf.ts
+++ src/services/smxkrf.ts
@@ -6,3 +6,4 @@ export const smxkrf = () => {
 // auto-update 2025-09-08T02:30:50.652Z
 // auto-update 2025-09-08T02:31:08.521Z
 // auto-update 2025-09-08T02:31:12.675Z
+// auto-update 2025-09-08T02:31:44.235Z
