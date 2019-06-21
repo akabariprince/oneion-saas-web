00d4b7eca57ec96b5ce02b79b759a2cf509ffea8
akabariprince <princeakabari123@gmail.com>
2019-06-21 16:59:00 +0530

    docs: update README

--- src/types/yshufq.ts
+++ src/types/yshufq.ts
@@ -13,3 +13,4 @@ export const yshufq = () => {
 // auto-update 2025-09-08T02:31:26.810Z
 // auto-update 2025-09-08T02:31:29.408Z
 // auto-update 2025-09-08T02:31:30.163Z
+// auto-update 2025-09-08T02:31:44.910Z
