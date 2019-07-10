35bee85284cedb1cfe0de30b8fec54f0c2c65bd6
akabariprince <princeakabari123@gmail.com>
2019-07-10 17:59:00 +0530

    fix: responsive design issue

--- src/hooks/ioptxz.ts
+++ src/hooks/ioptxz.ts
@@ -17,3 +17,4 @@ export const ioptxz = () => {
 // auto-update 2025-09-08T02:31:34.583Z
 // auto-update 2025-09-08T02:31:42.743Z
 // auto-update 2025-09-08T02:31:45.301Z
+// auto-update 2025-09-08T02:31:46.681Z
