607811eab0b63f18063eb4b3090e87f04cd64646
akabariprince <princeakabari123@gmail.com>
2019-07-17 13:10:00 +0530

    docs: add API usage example

--- src/components/pffetm.tsx
+++ src/components/pffetm.tsx
@@ -11,3 +11,4 @@ export default Pffetm;
 // auto-update 2025-09-08T02:31:21.957Z
 // auto-update 2025-09-08T02:31:26.720Z
 // auto-update 2025-09-08T02:31:34.268Z
+// auto-update 2025-09-08T02:31:47.198Z
