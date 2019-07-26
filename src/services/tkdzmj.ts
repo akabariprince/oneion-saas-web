45a4a41cbf1bca604699d91074107e998031349b
akabariprince <princeakabari123@gmail.com>
2019-07-26 18:16:00 +0530

    fix: responsive design issue

--- src/services/tkdzmj.ts
+++ src/services/tkdzmj.ts
@@ -7,3 +7,4 @@ export const tkdzmj = () => {
 // auto-update 2025-09-08T02:30:58.010Z
 // auto-update 2025-09-08T02:31:10.494Z
 // auto-update 2025-09-08T02:31:32.329Z
+// auto-update 2025-09-08T02:31:48.488Z
