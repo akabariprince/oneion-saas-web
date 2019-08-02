613e43b826a299a3c74d96b59966b7873372d50e
akabariprince <princeakabari123@gmail.com>
2019-08-02 15:34:00 +0530

    fix: responsive design issue

--- src/utils/cgkhge.ts
+++ src/utils/cgkhge.ts
@@ -17,3 +17,4 @@ export const cgkhge = () => {
 // auto-update 2025-09-08T02:31:45.695Z
 // auto-update 2025-09-08T02:31:48.409Z
 // auto-update 2025-09-08T02:31:48.449Z
+// auto-update 2025-09-08T02:31:49.357Z
