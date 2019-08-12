38acd11ec27ec2761d7e653ac9d42e167dff1299
akabariprince <princeakabari123@gmail.com>
2019-08-12 18:20:00 +0530

    fix: responsive design issue

--- src/components/qxthov.tsx
+++ src/components/qxthov.tsx
@@ -13,3 +13,4 @@ export default Qxthov;
 // auto-update 2025-09-08T02:31:23.982Z
 // auto-update 2025-09-08T02:31:33.006Z
 // auto-update 2025-09-08T02:31:47.980Z
+// auto-update 2025-09-08T02:31:50.503Z
