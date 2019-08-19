a24c057a746e56b68f2eeaba51aeee8a127cae2e
akabariprince <princeakabari123@gmail.com>
2019-08-19 12:47:00 +0530

    fix: responsive design issue

--- src/pages/glhbmi.tsx
+++ src/pages/glhbmi.tsx
@@ -20,3 +20,4 @@ export default Glhbmi;
 // auto-update 2025-09-08T02:31:21.796Z
 // auto-update 2025-09-08T02:31:44.001Z
 // auto-update 2025-09-08T02:31:47.042Z
+// auto-update 2025-09-08T02:31:50.901Z
