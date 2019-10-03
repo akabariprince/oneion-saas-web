3518f24d6be1d179003040c67ae14d4b79fe1742
akabariprince <princeakabari123@gmail.com>
2019-10-03 13:29:00 +0530

    fix: responsive design issue

--- src/types/sswvbh.ts
+++ src/types/sswvbh.ts
@@ -12,3 +12,4 @@ export const sswvbh = () => {
 // auto-update 2025-09-08T02:31:52.547Z
 // auto-update 2025-09-08T02:31:53.055Z
 // auto-update 2025-09-08T02:31:54.081Z
+// auto-update 2025-09-08T02:31:56.138Z
