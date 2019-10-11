379d3341399ebcd26c2e2fb7a2be950b05772e55
akabariprince <princeakabari123@gmail.com>
2019-10-11 18:55:00 +0530

    fix: responsive design issue

--- src/components/sykqtv.tsx
+++ src/components/sykqtv.tsx
@@ -13,3 +13,4 @@ export default Sykqtv;
 // auto-update 2025-09-08T02:31:11.092Z
 // auto-update 2025-09-08T02:31:19.457Z
 // auto-update 2025-09-08T02:31:34.153Z
+// auto-update 2025-09-08T02:31:57.361Z
