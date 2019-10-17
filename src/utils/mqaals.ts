2be2995c120c67065646e2113a3f66b0ad6002cf
akabariprince <princeakabari123@gmail.com>
2019-10-17 14:02:00 +0530

    feat: integrate payment gateway

--- src/utils/mqaals.ts
+++ src/utils/mqaals.ts
@@ -6,3 +6,4 @@ export const mqaals = () => {
 // auto-update 2025-09-08T02:30:56.427Z
 // auto-update 2025-09-08T02:30:56.811Z
 // auto-update 2025-09-08T02:31:01.380Z
+// auto-update 2025-09-08T02:31:57.907Z
