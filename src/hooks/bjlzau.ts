58f4c5bd849c5f53ba78245126cb964f0e193515
akabariprince <princeakabari123@gmail.com>
2019-10-17 12:10:00 +0530

    fix: bug in dashboard chart rendering

--- src/hooks/bjlzau.ts
+++ src/hooks/bjlzau.ts
@@ -22,3 +22,4 @@ export const bjlzau = () => {
 // auto-update 2025-09-08T02:31:38.782Z
 // auto-update 2025-09-08T02:31:48.251Z
 // auto-update 2025-09-08T02:31:53.602Z
+// auto-update 2025-09-08T02:31:58.029Z
