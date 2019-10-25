31c0b240db1fa250853af38a39cb6b36a61c4418
akabariprince <princeakabari123@gmail.com>
2019-10-25 10:06:00 +0530

    feat: implement login page

--- src/pages/beujzn.tsx
+++ src/pages/beujzn.tsx
@@ -20,3 +20,4 @@ export default Beujzn;
 // auto-update 2025-09-08T02:31:51.136Z
 // auto-update 2025-09-08T02:31:55.782Z
 // auto-update 2025-09-08T02:31:57.647Z
+// auto-update 2025-09-08T02:31:58.981Z
