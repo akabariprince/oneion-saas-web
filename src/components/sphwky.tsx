fd191311d520f326e44f11729bb8ed72c95138a5
akabariprince <princeakabari123@gmail.com>
2019-11-01 12:15:00 +0530

    fix: responsive design issue

--- src/components/sphwky.tsx
+++ src/components/sphwky.tsx
@@ -15,3 +15,4 @@ export default Sphwky;
 // auto-update 2025-09-08T02:31:13.500Z
 // auto-update 2025-09-08T02:31:20.447Z
 // auto-update 2025-09-08T02:31:49.239Z
+// auto-update 2025-09-08T02:31:59.946Z
