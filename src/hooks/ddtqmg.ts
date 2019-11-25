df1b5704fb79ed687b0ebaf6de62f1bdf1bc086c
akabariprince <princeakabari123@gmail.com>
2019-11-25 16:51:00 +0530

    fix: bug in dashboard chart rendering

--- src/hooks/ddtqmg.ts
+++ src/hooks/ddtqmg.ts
@@ -20,3 +20,4 @@ export const ddtqmg = () => {
 // auto-update 2025-09-08T02:31:54.955Z
 // auto-update 2025-09-08T02:31:56.807Z
 // auto-update 2025-09-08T02:31:57.949Z
+// auto-update 2025-09-08T02:32:02.772Z
