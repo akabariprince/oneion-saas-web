6fb741e9c4fe25dfbdb9c423b44be70af5c5f014
akabariprince <princeakabari123@gmail.com>
2019-11-27 14:03:00 +0530

    feat: add AI prediction API

--- src/services/ztesyr.ts
+++ src/services/ztesyr.ts
@@ -14,3 +14,4 @@ export const ztesyr = () => {
 // auto-update 2025-09-08T02:31:37.940Z
 // auto-update 2025-09-08T02:31:55.035Z
 // auto-update 2025-09-08T02:31:58.580Z
+// auto-update 2025-09-08T02:32:03.052Z
