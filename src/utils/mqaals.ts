cf2f856b671bde535df213feb7d18beeeeaf11c7
akabariprince <princeakabari123@gmail.com>
2020-01-28 13:42:00 +0530

    docs: add API usage example

--- src/utils/mqaals.ts
+++ src/utils/mqaals.ts
@@ -9,3 +9,4 @@ export const mqaals = () => {
 // auto-update 2025-09-08T02:31:57.907Z
 // auto-update 2025-09-08T02:31:58.068Z
 // auto-update 2025-09-08T02:32:06.723Z
+// auto-update 2025-09-08T02:32:09.717Z
