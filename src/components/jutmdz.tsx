4ba64d3684590e3abc3f9ef7da114d0c1240be4a
akabariprince <princeakabari123@gmail.com>
2020-02-11 18:30:00 +0530

    fix: bug in dashboard chart rendering

--- src/components/jutmdz.tsx
+++ src/components/jutmdz.tsx
@@ -18,3 +18,4 @@ export default Jutmdz;
 // auto-update 2025-09-08T02:31:51.568Z
 // auto-update 2025-09-08T02:31:51.855Z
 // auto-update 2025-09-08T02:32:05.729Z
+// auto-update 2025-09-08T02:32:11.112Z
