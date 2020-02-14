f0d6a6813a41f9e014ad535ee37015e8cf0c3671
akabariprince <princeakabari123@gmail.com>
2020-02-14 11:29:00 +0530

    fix: responsive design issue

--- src/services/hitzup.ts
+++ src/services/hitzup.ts
@@ -7,3 +7,4 @@ export const hitzup = () => {
 // auto-update 2025-09-08T02:31:04.834Z
 // auto-update 2025-09-08T02:31:11.445Z
 // auto-update 2025-09-08T02:31:12.635Z
+// auto-update 2025-09-08T02:32:11.667Z
