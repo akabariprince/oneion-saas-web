f42cfc3463c8cd1b1ea0846d3486ffbeddab2198
akabariprince <princeakabari123@gmail.com>
2020-03-05 10:10:00 +0530

    fix: responsive design issue

--- src/pages/cblbye.tsx
+++ src/pages/cblbye.tsx
@@ -17,3 +17,4 @@ export default Cblbye;
 // auto-update 2025-09-08T02:32:04.570Z
 // auto-update 2025-09-08T02:32:12.351Z
 // auto-update 2025-09-08T02:32:13.837Z
+// auto-update 2025-09-08T02:32:14.118Z
