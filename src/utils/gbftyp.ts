b0fc702fa1ec916f6628619f655d5eccabb8a1b1
akabariprince <princeakabari123@gmail.com>
2020-03-11 11:35:00 +0530

    fix: handle null pointer in API

--- src/utils/gbftyp.ts
+++ src/utils/gbftyp.ts
@@ -16,3 +16,4 @@ export const gbftyp = () => {
 // auto-update 2025-09-08T02:31:42.582Z
 // auto-update 2025-09-08T02:31:57.165Z
 // auto-update 2025-09-08T02:32:01.414Z
+// auto-update 2025-09-08T02:32:14.797Z
