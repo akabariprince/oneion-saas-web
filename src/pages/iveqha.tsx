3f9f6756bddec231a687f701f4cd9237888ca60d
akabariprince <princeakabari123@gmail.com>
2020-04-03 12:42:00 +0530

    docs: add API usage example

--- src/pages/iveqha.tsx
+++ src/pages/iveqha.tsx
@@ -15,3 +15,4 @@ export default Iveqha;
 // auto-update 2025-09-08T02:31:37.112Z
 // auto-update 2025-09-08T02:31:53.802Z
 // auto-update 2025-09-08T02:32:04.291Z
+// auto-update 2025-09-08T02:32:17.432Z
