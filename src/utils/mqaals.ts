d0ffb010479fa5299daddbe57613cfa2acc0bb65
akabariprince <princeakabari123@gmail.com>
2020-04-14 10:07:00 +0530

    fix: handle null pointer in API

--- src/utils/mqaals.ts
+++ src/utils/mqaals.ts
@@ -12,3 +12,4 @@ export const mqaals = () => {
 // auto-update 2025-09-08T02:32:09.717Z
 // auto-update 2025-09-08T02:32:15.754Z
 // auto-update 2025-09-08T02:32:16.430Z
+// auto-update 2025-09-08T02:32:18.592Z
