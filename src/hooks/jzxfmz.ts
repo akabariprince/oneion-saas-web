039b4702d302dce8ca31007c7d6bb6d48234c354
akabariprince <princeakabari123@gmail.com>
2020-04-14 09:55:00 +0530

    fix: handle null pointer in API

--- src/hooks/jzxfmz.ts
+++ src/hooks/jzxfmz.ts
@@ -19,3 +19,4 @@ export const jzxfmz = () => {
 // auto-update 2025-09-08T02:31:55.983Z
 // auto-update 2025-09-08T02:32:02.174Z
 // auto-update 2025-09-08T02:32:10.014Z
+// auto-update 2025-09-08T02:32:18.632Z
