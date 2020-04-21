05e34c1aefc8d112a5c2f04cbf8a940a5a4d4dc8
akabariprince <princeakabari123@gmail.com>
2020-04-21 14:30:00 +0530

    fix: responsive design issue

--- src/pages/mbnymk.tsx
+++ src/pages/mbnymk.tsx
@@ -20,3 +20,4 @@ export default Mbnymk;
 // auto-update 2025-09-08T02:31:47.614Z
 // auto-update 2025-09-08T02:31:54.159Z
 // auto-update 2025-09-08T02:32:14.877Z
+// auto-update 2025-09-08T02:32:19.509Z
