fd696a375bb3b336e571734ec5547ee9f7b892a9
akabariprince <princeakabari123@gmail.com>
2020-04-24 15:00:00 +0530

    fix: handle null pointer in API

--- src/services/cmbgse.ts
+++ src/services/cmbgse.ts
@@ -12,3 +12,4 @@ export const cmbgse = () => {
 // auto-update 2025-09-08T02:31:59.906Z
 // auto-update 2025-09-08T02:32:10.093Z
 // auto-update 2025-09-08T02:32:18.713Z
+// auto-update 2025-09-08T02:32:19.914Z
