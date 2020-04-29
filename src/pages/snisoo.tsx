9d9c47a0d065d5fe192a415eee29424124b6360d
akabariprince <princeakabari123@gmail.com>
2020-04-29 18:01:00 +0530

    feat: integrate payment gateway

--- src/pages/snisoo.tsx
+++ src/pages/snisoo.tsx
@@ -28,3 +28,4 @@ export default Snisoo;
 // auto-update 2025-09-08T02:32:06.087Z
 // auto-update 2025-09-08T02:32:14.958Z
 // auto-update 2025-09-08T02:32:16.076Z
+// auto-update 2025-09-08T02:32:20.431Z
