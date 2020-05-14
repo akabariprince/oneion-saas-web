2f9522d02f390b79e60614b2701715d6f9d1ac61
akabariprince <princeakabari123@gmail.com>
2020-05-14 17:23:00 +0530

    feat: integrate payment gateway

--- src/services/rbnmja.ts
+++ src/services/rbnmja.ts
@@ -8,3 +8,4 @@ export const rbnmja = () => {
 // auto-update 2025-09-08T02:31:45.262Z
 // auto-update 2025-09-08T02:32:08.759Z
 // auto-update 2025-09-08T02:32:21.472Z
+// auto-update 2025-09-08T02:32:22.313Z
