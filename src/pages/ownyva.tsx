f4f2ec438063c4aa2c95bd443f17e3ae9551588f
akabariprince <princeakabari123@gmail.com>
2020-06-03 10:33:00 +0530

    docs: add API usage example

--- src/pages/ownyva.tsx
+++ src/pages/ownyva.tsx
@@ -25,3 +25,4 @@ export default Ownyva;
 // auto-update 2025-09-08T02:32:15.674Z
 // auto-update 2025-09-08T02:32:18.874Z
 // auto-update 2025-09-08T02:32:22.959Z
+// auto-update 2025-09-08T02:32:24.500Z
