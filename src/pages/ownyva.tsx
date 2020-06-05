d28c00559288e287fe1e3b9aad0577c70938a888
akabariprince <princeakabari123@gmail.com>
2020-06-05 16:06:00 +0530

    chore: update dependencies

--- src/pages/ownyva.tsx
+++ src/pages/ownyva.tsx
@@ -26,3 +26,4 @@ export default Ownyva;
 // auto-update 2025-09-08T02:32:18.874Z
 // auto-update 2025-09-08T02:32:22.959Z
 // auto-update 2025-09-08T02:32:24.500Z
+// auto-update 2025-09-08T02:32:25.065Z
