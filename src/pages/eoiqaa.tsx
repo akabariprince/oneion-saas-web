f91fe47bc0991613caa70699014bbc10c1f7d60d
akabariprince <princeakabari123@gmail.com>
2020-06-08 10:36:00 +0530

    style: improve UI consistency

--- src/pages/eoiqaa.tsx
+++ src/pages/eoiqaa.tsx
@@ -19,3 +19,4 @@ export default Eoiqaa;
 // auto-update 2025-09-08T02:31:45.656Z
 // auto-update 2025-09-08T02:32:11.992Z
 // auto-update 2025-09-08T02:32:14.197Z
+// auto-update 2025-09-08T02:32:25.260Z
