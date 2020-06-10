db8f268fb211dc8dc757742baf179bdd2b865364
akabariprince <princeakabari123@gmail.com>
2020-06-10 10:19:00 +0530

    docs: add API usage example

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -21,3 +21,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:32:10.328Z
 // auto-update 2025-09-08T02:32:14.355Z
 // auto-update 2025-09-08T02:32:21.273Z
+// auto-update 2025-09-08T02:32:25.580Z
