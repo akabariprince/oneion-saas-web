37cec98c19d77c29b060294775382fcd3ac72e31
akabariprince <princeakabari123@gmail.com>
2020-06-30 14:00:00 +0530

    feat: integrate payment gateway

--- src/pages/bllisq.tsx
+++ src/pages/bllisq.tsx
@@ -24,3 +24,4 @@ export default Bllisq;
 // auto-update 2025-09-08T02:32:17.877Z
 // auto-update 2025-09-08T02:32:22.879Z
 // auto-update 2025-09-08T02:32:26.622Z
+// auto-update 2025-09-08T02:32:27.145Z
