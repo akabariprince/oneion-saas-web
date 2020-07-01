7a98941e3433cc132ed38db74f7ace47899367d5
akabariprince <princeakabari123@gmail.com>
2020-07-01 11:54:00 +0530

    feat: integrate payment gateway

--- src/types/kamxzl.ts
+++ src/types/kamxzl.ts
@@ -24,3 +24,4 @@ export const kamxzl = () => {
 // auto-update 2025-09-08T02:32:19.711Z
 // auto-update 2025-09-08T02:32:24.262Z
 // auto-update 2025-09-08T02:32:25.741Z
+// auto-update 2025-09-08T02:32:27.463Z
