91be3571d5cecd27514d13ab1b681c8c7437ca35
akabariprince <princeakabari123@gmail.com>
2020-08-05 12:15:00 +0530

    fix: responsive design issue

--- src/utils/ayrqkx.ts
+++ src/utils/ayrqkx.ts
@@ -25,3 +25,4 @@ export const ayrqkx = () => {
 // auto-update 2025-09-08T02:32:02.892Z
 // auto-update 2025-09-08T02:32:08.438Z
 // auto-update 2025-09-08T02:32:27.745Z
+// auto-update 2025-09-08T02:32:30.027Z
