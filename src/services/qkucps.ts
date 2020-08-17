4c400913c7f249e030fbd38aec69b63f4a4e1921
akabariprince <princeakabari123@gmail.com>
2020-08-17 14:19:00 +0530

    fix: responsive design issue

--- src/services/qkucps.ts
+++ src/services/qkucps.ts
@@ -10,3 +10,4 @@ export const qkucps = () => {
 // auto-update 2025-09-08T02:31:11.964Z
 // auto-update 2025-09-08T02:32:02.410Z
 // auto-update 2025-09-08T02:32:11.348Z
+// auto-update 2025-09-08T02:32:31.466Z
