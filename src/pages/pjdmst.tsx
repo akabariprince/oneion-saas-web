6c3bd876c35626576a5db27e7e88c3e72b57ce74
akabariprince <princeakabari123@gmail.com>
2020-08-19 10:35:00 +0530

    fix: handle null pointer in API

--- src/pages/pjdmst.tsx
+++ src/pages/pjdmst.tsx
@@ -26,3 +26,4 @@ export default Pjdmst;
 // auto-update 2025-09-08T02:32:22.432Z
 // auto-update 2025-09-08T02:32:27.908Z
 // auto-update 2025-09-08T02:32:28.947Z
+// auto-update 2025-09-08T02:32:31.750Z
