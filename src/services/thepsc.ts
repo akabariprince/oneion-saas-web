77954a4d50dfd9d24e334ee492ac68a53c889fb5
akabariprince <princeakabari123@gmail.com>
2020-08-19 18:39:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/thepsc.ts
+++ src/services/thepsc.ts
@@ -13,3 +13,4 @@ export const thepsc = () => {
 // auto-update 2025-09-08T02:31:42.900Z
 // auto-update 2025-09-08T02:32:19.469Z
 // auto-update 2025-09-08T02:32:22.472Z
+// auto-update 2025-09-08T02:32:31.709Z
