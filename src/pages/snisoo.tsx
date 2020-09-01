7a24577c7c1a5565eda3b6073710705c6fd7c027
akabariprince <princeakabari123@gmail.com>
2020-09-01 11:34:00 +0530

    docs: add API usage example

--- src/pages/snisoo.tsx
+++ src/pages/snisoo.tsx
@@ -32,3 +32,4 @@ export default Snisoo;
 // auto-update 2025-09-08T02:32:26.026Z
 // auto-update 2025-09-08T02:32:28.108Z
 // auto-update 2025-09-08T02:32:28.624Z
+// auto-update 2025-09-08T02:32:33.205Z
