c75deeac9030e23d586ca5a838611c3411319547
akabariprince <princeakabari123@gmail.com>
2020-09-04 15:29:00 +0530

    fix: responsive design issue

--- src/hooks/ankglw.ts
+++ src/hooks/ankglw.ts
@@ -23,3 +23,4 @@ export const ankglw = () => {
 // auto-update 2025-09-08T02:32:25.621Z
 // auto-update 2025-09-08T02:32:26.987Z
 // auto-update 2025-09-08T02:32:27.949Z
+// auto-update 2025-09-08T02:32:33.631Z
