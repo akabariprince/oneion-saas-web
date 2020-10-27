ee016982ab6073b2ae5d1ae06b58bec4249a4dbe
akabariprince <princeakabari123@gmail.com>
2020-10-27 15:30:00 +0530

    docs: add API usage example

--- src/pages/ogxbau.tsx
+++ src/pages/ogxbau.tsx
@@ -23,3 +23,4 @@ export default Ogxbau;
 // auto-update 2025-09-08T02:32:27.106Z
 // auto-update 2025-09-08T02:32:31.841Z
 // auto-update 2025-09-08T02:32:34.160Z
+// auto-update 2025-09-08T02:32:39.966Z
