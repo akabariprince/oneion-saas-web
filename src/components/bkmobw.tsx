a050ae58eaf6b8f94ba5f751288a90b7478f68b6
akabariprince <princeakabari123@gmail.com>
2020-12-04 15:02:00 +0530

    fix: responsive design issue

--- src/components/bkmobw.tsx
+++ src/components/bkmobw.tsx
@@ -19,3 +19,4 @@ export default Bkmobw;
 // auto-update 2025-09-08T02:31:55.584Z
 // auto-update 2025-09-08T02:32:06.802Z
 // auto-update 2025-09-08T02:32:12.391Z
+// auto-update 2025-09-08T02:32:45.040Z
