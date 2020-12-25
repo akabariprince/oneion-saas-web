230b0768584f120171cc6fd98785713a593e327e
akabariprince <princeakabari123@gmail.com>
2020-12-25 13:39:00 +0530

    test: add unit tests for auth

--- src/types/xrucwy.ts
+++ src/types/xrucwy.ts
@@ -28,3 +28,4 @@ export const xrucwy = () => {
 // auto-update 2025-09-08T02:32:38.528Z
 // auto-update 2025-09-08T02:32:43.164Z
 // auto-update 2025-09-08T02:32:44.959Z
+// auto-update 2025-09-08T02:32:47.788Z
