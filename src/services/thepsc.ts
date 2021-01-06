41213cfee65a05d9df86d29121791ffdce8fe2ff
akabariprince <princeakabari123@gmail.com>
2021-01-06 10:45:00 +0530

    feat: integrate payment gateway

--- src/services/thepsc.ts
+++ src/services/thepsc.ts
@@ -15,3 +15,4 @@ export const thepsc = () => {
 // auto-update 2025-09-08T02:32:22.472Z
 // auto-update 2025-09-08T02:32:31.709Z
 // auto-update 2025-09-08T02:32:38.291Z
+// auto-update 2025-09-08T02:32:49.128Z
