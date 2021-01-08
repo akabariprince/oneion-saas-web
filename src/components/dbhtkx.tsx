85b18120ebf11368f6359f8783574aff41e8a40b
akabariprince <princeakabari123@gmail.com>
2021-01-08 17:08:00 +0530

    fix: responsive design issue

--- src/components/dbhtkx.tsx
+++ src/components/dbhtkx.tsx
@@ -20,3 +20,4 @@ export default Dbhtkx;
 // auto-update 2025-09-08T02:32:16.272Z
 // auto-update 2025-09-08T02:32:24.222Z
 // auto-update 2025-09-08T02:32:43.047Z
+// auto-update 2025-09-08T02:32:49.284Z
