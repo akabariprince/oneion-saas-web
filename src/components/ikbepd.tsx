b15bd9e1079f950eb69836da6f1ddeb672bd1ff3
akabariprince <princeakabari123@gmail.com>
2021-01-12 19:43:00 +0530

    docs: add API usage example

--- src/components/ikbepd.tsx
+++ src/components/ikbepd.tsx
@@ -14,3 +14,4 @@ export default Ikbepd;
 // auto-update 2025-09-08T02:31:55.703Z
 // auto-update 2025-09-08T02:32:13.514Z
 // auto-update 2025-09-08T02:32:33.427Z
+// auto-update 2025-09-08T02:32:49.847Z
