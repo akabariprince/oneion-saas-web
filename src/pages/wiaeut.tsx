adab699fafb94e9ab4844cbaac883dd731502d96
akabariprince <princeakabari123@gmail.com>
2021-02-08 11:15:00 +0530

    fix: handle null pointer in API

--- src/pages/wiaeut.tsx
+++ src/pages/wiaeut.tsx
@@ -29,3 +29,4 @@ export default Wiaeut;
 // auto-update 2025-09-08T02:32:31.587Z
 // auto-update 2025-09-08T02:32:35.117Z
 // auto-update 2025-09-08T02:32:40.163Z
+// auto-update 2025-09-08T02:32:53.073Z
