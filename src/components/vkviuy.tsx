eccdbfeaed74209c78a8309ca969abf41f7508ae
akabariprince <princeakabari123@gmail.com>
2021-02-22 17:51:00 +0530

    fix: responsive design issue

--- src/components/vkviuy.tsx
+++ src/components/vkviuy.tsx
@@ -17,3 +17,4 @@ export default Vkviuy;
 // auto-update 2025-09-08T02:32:11.708Z
 // auto-update 2025-09-08T02:32:13.038Z
 // auto-update 2025-09-08T02:32:29.067Z
+// auto-update 2025-09-08T02:32:54.953Z
