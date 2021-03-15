7279f02a498eb1787837cea06a9f9fc4201d082e
akabariprince <princeakabari123@gmail.com>
2021-03-15 10:26:00 +0530

    test: add unit tests for auth

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -38,3 +38,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:32:47.467Z
 // auto-update 2025-09-08T02:32:48.070Z
 // auto-update 2025-09-08T02:32:51.524Z
+// auto-update 2025-09-08T02:32:56.709Z
