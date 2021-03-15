57230d2e18230cbd6a60e4a581523b3b68773089
akabariprince <princeakabari123@gmail.com>
2021-03-15 14:53:00 +0530

    fix: responsive design issue

--- src/services/ejykcx.ts
+++ src/services/ejykcx.ts
@@ -10,3 +10,4 @@ export const ejykcx = () => {
 // auto-update 2025-09-08T02:32:02.292Z
 // auto-update 2025-09-08T02:32:08.163Z
 // auto-update 2025-09-08T02:32:12.674Z
+// auto-update 2025-09-08T02:32:56.669Z
