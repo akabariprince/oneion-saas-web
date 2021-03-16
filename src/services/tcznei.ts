2f393cb99f670743afd3727f47a8ce24d9985c6e
akabariprince <princeakabari123@gmail.com>
2021-03-16 18:49:00 +0530

    feat: integrate payment gateway

--- src/services/tcznei.ts
+++ src/services/tcznei.ts
@@ -25,3 +25,4 @@ export const tcznei = () => {
 // auto-update 2025-09-08T02:32:30.623Z
 // auto-update 2025-09-08T02:32:34.555Z
 // auto-update 2025-09-08T02:32:53.510Z
+// auto-update 2025-09-08T02:32:56.912Z
