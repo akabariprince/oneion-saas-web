fee87870d7c8a5cead7ade00a0770afd6f5c913f
akabariprince <princeakabari123@gmail.com>
2021-04-06 18:03:00 +0530

    fix: bug in dashboard chart rendering

--- src/hooks/jrcanm.ts
+++ src/hooks/jrcanm.ts
@@ -29,3 +29,4 @@ export const jrcanm = () => {
 // auto-update 2025-09-08T02:32:42.288Z
 // auto-update 2025-09-08T02:32:50.524Z
 // auto-update 2025-09-08T02:32:53.470Z
+// auto-update 2025-09-08T02:32:58.957Z
