d4955742166a97fef57b851fc608c81009f31ae6
akabariprince <princeakabari123@gmail.com>
2021-04-07 15:40:00 +0530

    fix: bug in dashboard chart rendering

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -33,3 +33,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:32:18.914Z
 // auto-update 2025-09-08T02:32:40.124Z
 // auto-update 2025-09-08T02:32:43.803Z
+// auto-update 2025-09-08T02:32:59.117Z
