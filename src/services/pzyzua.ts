3a86683bddd9f887549c456c55d776631a3d8001
akabariprince <princeakabari123@gmail.com>
2021-04-08 12:24:00 +0530

    test: add unit tests for auth

--- src/services/pzyzua.ts
+++ src/services/pzyzua.ts
@@ -14,3 +14,4 @@ export const pzyzua = () => {
 // auto-update 2025-09-08T02:32:28.584Z
 // auto-update 2025-09-08T02:32:43.641Z
 // auto-update 2025-09-08T02:32:46.466Z
+// auto-update 2025-09-08T02:32:59.474Z
