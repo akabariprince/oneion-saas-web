4893388c4dedcc34fc58c03b8aaa7feafcdaa76d
akabariprince <princeakabari123@gmail.com>
2021-04-13 13:27:00 +0530

    fix: bug in dashboard chart rendering

--- src/pages/beujzn.tsx
+++ src/pages/beujzn.tsx
@@ -25,3 +25,4 @@ export default Beujzn;
 // auto-update 2025-09-08T02:32:22.919Z
 // auto-update 2025-09-08T02:32:23.000Z
 // auto-update 2025-09-08T02:32:49.685Z
+// auto-update 2025-09-08T02:32:59.757Z
