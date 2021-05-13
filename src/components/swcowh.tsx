cbe9b64373946cc321e56afc4964bd0536b41e65
akabariprince <princeakabari123@gmail.com>
2021-05-13 13:32:00 +0530

    fix: bug in dashboard chart rendering

--- src/components/swcowh.tsx
+++ src/components/swcowh.tsx
@@ -22,3 +22,4 @@ export default Swcowh;
 // auto-update 2025-09-08T02:32:55.911Z
 // auto-update 2025-09-08T02:32:56.111Z
 // auto-update 2025-09-08T02:33:00.081Z
+// auto-update 2025-09-08T02:33:03.958Z
