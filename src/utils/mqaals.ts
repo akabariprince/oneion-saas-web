c5cac8fdc9afcdced573b931e91c593e868e9959
akabariprince <princeakabari123@gmail.com>
2021-06-07 11:54:00 +0530

    fix: bug in dashboard chart rendering

--- src/utils/mqaals.ts
+++ src/utils/mqaals.ts
@@ -19,3 +19,4 @@ export const mqaals = () => {
 // auto-update 2025-09-08T02:32:56.073Z
 // auto-update 2025-09-08T02:33:02.744Z
 // auto-update 2025-09-08T02:33:03.917Z
+// auto-update 2025-09-08T02:33:06.690Z
