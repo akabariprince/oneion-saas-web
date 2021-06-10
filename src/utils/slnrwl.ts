00c56ee860e06f6bb15b89dea848d3c2c517a796
akabariprince <princeakabari123@gmail.com>
2021-06-10 15:47:00 +0530

    fix: handle null pointer in API

--- src/utils/slnrwl.ts
+++ src/utils/slnrwl.ts
@@ -28,3 +28,4 @@ export const slnrwl = () => {
 // auto-update 2025-09-08T02:32:58.115Z
 // auto-update 2025-09-08T02:33:00.318Z
 // auto-update 2025-09-08T02:33:06.771Z
+// auto-update 2025-09-08T02:33:07.255Z
