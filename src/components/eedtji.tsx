5cec1461e57766e0f00195d44b3c86adeb7298dd
akabariprince <princeakabari123@gmail.com>
2021-06-11 17:01:00 +0530

    feat: integrate payment gateway

--- src/components/eedtji.tsx
+++ src/components/eedtji.tsx
@@ -13,3 +13,4 @@ export default Eedtji;
 // auto-update 2025-09-08T02:32:25.420Z
 // auto-update 2025-09-08T02:32:45.235Z
 // auto-update 2025-09-08T02:32:46.387Z
+// auto-update 2025-09-08T02:33:07.574Z
