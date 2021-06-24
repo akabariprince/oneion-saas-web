4c75e95f641c0e1d6bad02fe9fe96cd1f9653472
akabariprince <princeakabari123@gmail.com>
2021-06-24 17:53:00 +0530

    docs: add API usage example

--- src/services/owimho.ts
+++ src/services/owimho.ts
@@ -14,3 +14,4 @@ export const owimho = () => {
 // auto-update 2025-09-08T02:32:43.442Z
 // auto-update 2025-09-08T02:32:57.231Z
 // auto-update 2025-09-08T02:32:58.037Z
+// auto-update 2025-09-08T02:33:08.902Z
