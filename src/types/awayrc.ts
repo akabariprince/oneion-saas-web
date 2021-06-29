34b37c09571735ae2fc261495e228b091e8df986
akabariprince <princeakabari123@gmail.com>
2021-06-29 09:15:00 +0530

    fix: responsive design issue

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -25,3 +25,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:32:58.673Z
 // auto-update 2025-09-08T02:33:01.781Z
 // auto-update 2025-09-08T02:33:04.809Z
+// auto-update 2025-09-08T02:33:09.502Z
