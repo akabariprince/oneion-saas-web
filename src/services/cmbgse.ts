5f16798bca88a004f2da985ab211b2c59b9aaf5d
akabariprince <princeakabari123@gmail.com>
2021-07-08 13:35:00 +0530

    refactor: optimize API service

--- src/services/cmbgse.ts
+++ src/services/cmbgse.ts
@@ -21,3 +21,4 @@ export const cmbgse = () => {
 // auto-update 2025-09-08T02:32:52.206Z
 // auto-update 2025-09-08T02:32:53.750Z
 // auto-update 2025-09-08T02:33:05.091Z
+// auto-update 2025-09-08T02:33:10.607Z
