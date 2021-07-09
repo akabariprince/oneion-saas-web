b80d48a45b6966c8cf050cb2a5046029ed636a98
akabariprince <princeakabari123@gmail.com>
2021-07-09 17:29:00 +0530

    feat: integrate payment gateway

--- src/utils/ayrqkx.ts
+++ src/utils/ayrqkx.ts
@@ -36,3 +36,4 @@ export const ayrqkx = () => {
 // auto-update 2025-09-08T02:33:03.464Z
 // auto-update 2025-09-08T02:33:06.933Z
 // auto-update 2025-09-08T02:33:10.567Z
+// auto-update 2025-09-08T02:33:10.769Z
