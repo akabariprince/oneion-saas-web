a548e38f2df197e44a1353e127762fbf059f1143
akabariprince <princeakabari123@gmail.com>
2021-07-14 14:42:00 +0530

    fix: responsive design issue

--- src/services/owimho.ts
+++ src/services/owimho.ts
@@ -15,3 +15,4 @@ export const owimho = () => {
 // auto-update 2025-09-08T02:32:57.231Z
 // auto-update 2025-09-08T02:32:58.037Z
 // auto-update 2025-09-08T02:33:08.902Z
+// auto-update 2025-09-08T02:33:11.370Z
