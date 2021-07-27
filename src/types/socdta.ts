e666ed66e2de6b4d971cbc398a9dfa9a58cbc2b7
akabariprince <princeakabari123@gmail.com>
2021-07-27 18:00:00 +0530

    style: improve UI consistency

--- src/types/socdta.ts
+++ src/types/socdta.ts
@@ -35,3 +35,4 @@ export const socdta = () => {
 // auto-update 2025-09-08T02:33:00.043Z
 // auto-update 2025-09-08T02:33:01.661Z
 // auto-update 2025-09-08T02:33:03.754Z
+// auto-update 2025-09-08T02:33:12.985Z
