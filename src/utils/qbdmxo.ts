65568378898d701e0e83304ace4ee10904740035
akabariprince <princeakabari123@gmail.com>
2021-08-20 09:02:00 +0530

    fix: handle null pointer in API

--- src/utils/qbdmxo.ts
+++ src/utils/qbdmxo.ts
@@ -34,3 +34,4 @@ export const qbdmxo = () => {
 // auto-update 2025-09-08T02:32:59.798Z
 // auto-update 2025-09-08T02:33:02.704Z
 // auto-update 2025-09-08T02:33:11.977Z
+// auto-update 2025-09-08T02:33:15.603Z
