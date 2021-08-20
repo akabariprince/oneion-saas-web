3aaf58246c71540fe3bb4fb0f3883b874442241f
akabariprince <princeakabari123@gmail.com>
2021-08-20 13:43:00 +0530

    fix: handle null pointer in API

--- src/types/kxadhe.ts
+++ src/types/kxadhe.ts
@@ -40,3 +40,4 @@ export const kxadhe = () => {
 // auto-update 2025-09-08T02:33:06.134Z
 // auto-update 2025-09-08T02:33:14.105Z
 // auto-update 2025-09-08T02:33:15.522Z
+// auto-update 2025-09-08T02:33:15.562Z
