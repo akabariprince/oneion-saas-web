bd7f7b9fc944c52d2045fca31ffe512c6f5c7ac8
akabariprince <princeakabari123@gmail.com>
2021-09-01 16:12:00 +0530

    feat: integrate payment gateway

--- src/pages/yqtylt.tsx
+++ src/pages/yqtylt.tsx
@@ -26,3 +26,4 @@ export default Yqtylt;
 // auto-update 2025-09-08T02:32:28.345Z
 // auto-update 2025-09-08T02:32:43.243Z
 // auto-update 2025-09-08T02:32:48.148Z
+// auto-update 2025-09-08T02:33:17.048Z
