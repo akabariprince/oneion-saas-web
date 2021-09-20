697bb179a49ef4ee5b37a2d36e8787fd9156738a
akabariprince <princeakabari123@gmail.com>
2021-09-20 17:41:00 +0530

    fix: bug in dashboard chart rendering

--- src/types/kamxzl.ts
+++ src/types/kamxzl.ts
@@ -35,3 +35,4 @@ export const kamxzl = () => {
 // auto-update 2025-09-08T02:32:54.590Z
 // auto-update 2025-09-08T02:33:01.701Z
 // auto-update 2025-09-08T02:33:14.886Z
+// auto-update 2025-09-08T02:33:19.278Z
