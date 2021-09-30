b8f2297639b80e2b2d93a910110eb35258de7cf5
akabariprince <princeakabari123@gmail.com>
2021-09-30 09:19:00 +0530

    fix: responsive design issue

--- src/hooks/xpiwwp.ts
+++ src/hooks/xpiwwp.ts
@@ -30,3 +30,4 @@ export const xpiwwp = () => {
 // auto-update 2025-09-08T02:32:55.229Z
 // auto-update 2025-09-08T02:33:06.330Z
 // auto-update 2025-09-08T02:33:16.683Z
+// auto-update 2025-09-08T02:33:20.849Z
