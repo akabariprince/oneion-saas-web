2b5e7db8abe9e5bec72d49f88111ccbb38caa1f0
akabariprince <princeakabari123@gmail.com>
2021-09-30 13:27:00 +0530

    feat: add AI prediction API

--- src/types/thrsdg.ts
+++ src/types/thrsdg.ts
@@ -37,3 +37,4 @@ export const thrsdg = () => {
 // auto-update 2025-09-08T02:33:14.382Z
 // auto-update 2025-09-08T02:33:14.603Z
 // auto-update 2025-09-08T02:33:19.399Z
+// auto-update 2025-09-08T02:33:20.930Z
