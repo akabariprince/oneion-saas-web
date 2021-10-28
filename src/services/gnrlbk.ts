a2c52609e1472c198c500b311a04fe95d9e3989b
akabariprince <princeakabari123@gmail.com>
2021-10-28 16:46:00 +0530

    docs: add API usage example

--- src/services/gnrlbk.ts
+++ src/services/gnrlbk.ts
@@ -5,3 +5,4 @@ export const gnrlbk = () => {
 // auto-update 2025-09-08T02:31:46.841Z
 // auto-update 2025-09-08T02:32:12.472Z
 // auto-update 2025-09-08T02:32:48.506Z
+// auto-update 2025-09-08T02:33:24.115Z
