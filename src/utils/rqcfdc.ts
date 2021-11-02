c3569319173b918659b63b7fd9384f07ca6738ed
akabariprince <princeakabari123@gmail.com>
2021-11-02 14:55:00 +0530

    feat: integrate payment gateway

--- src/utils/rqcfdc.ts
+++ src/utils/rqcfdc.ts
@@ -39,3 +39,4 @@ export const rqcfdc = () => {
 // auto-update 2025-09-08T02:33:09.542Z
 // auto-update 2025-09-08T02:33:13.182Z
 // auto-update 2025-09-08T02:33:21.933Z
+// auto-update 2025-09-08T02:33:24.474Z
