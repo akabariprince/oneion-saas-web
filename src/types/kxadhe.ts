286e7b691eeeca80714da4ba5069a77d02439bfd
akabariprince <princeakabari123@gmail.com>
2021-11-12 09:40:00 +0530

    docs: add API usage example

--- src/types/kxadhe.ts
+++ src/types/kxadhe.ts
@@ -42,3 +42,4 @@ export const kxadhe = () => {
 // auto-update 2025-09-08T02:33:15.522Z
 // auto-update 2025-09-08T02:33:15.562Z
 // auto-update 2025-09-08T02:33:21.811Z
+// auto-update 2025-09-08T02:33:25.853Z
