0a6b10b81a1713bd53fe371c10ea777f10000527
akabariprince <princeakabari123@gmail.com>
2021-11-17 18:36:00 +0530

    fix: handle null pointer in API

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -44,3 +44,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:33:02.865Z
 // auto-update 2025-09-08T02:33:19.520Z
 // auto-update 2025-09-08T02:33:20.525Z
+// auto-update 2025-09-08T02:33:26.298Z
