18b3edcac232bc06f03080ab41001dd41e05b1be
akabariprince <princeakabari123@gmail.com>
2021-12-09 11:27:00 +0530

    docs: add API usage example

--- src/utils/gbftyp.ts
+++ src/utils/gbftyp.ts
@@ -35,3 +35,4 @@ export const gbftyp = () => {
 // auto-update 2025-09-08T02:33:20.364Z
 // auto-update 2025-09-08T02:33:24.677Z
 // auto-update 2025-09-08T02:33:27.017Z
+// auto-update 2025-09-08T02:33:29.452Z
