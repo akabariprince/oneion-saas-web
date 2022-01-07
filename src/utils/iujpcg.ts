35a5b9069061fee7597de4115b92028a4168b105
akabariprince <princeakabari123@gmail.com>
2022-01-07 17:43:00 +0530

    test: add unit tests for auth

--- src/utils/iujpcg.ts
+++ src/utils/iujpcg.ts
@@ -34,3 +34,4 @@ export const iujpcg = () => {
 // auto-update 2025-09-08T02:33:25.158Z
 // auto-update 2025-09-08T02:33:25.513Z
 // auto-update 2025-09-08T02:33:26.619Z
+// auto-update 2025-09-08T02:33:31.403Z
