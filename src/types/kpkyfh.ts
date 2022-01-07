292d2fae430cef04b7cfc3760d631e3cc6bebeaf
akabariprince <princeakabari123@gmail.com>
2022-01-07 10:21:00 +0530

    fix: bug in dashboard chart rendering

--- src/types/kpkyfh.ts
+++ src/types/kpkyfh.ts
@@ -38,3 +38,4 @@ export const kpkyfh = () => {
 // auto-update 2025-09-08T02:33:07.899Z
 // auto-update 2025-09-08T02:33:22.734Z
 // auto-update 2025-09-08T02:33:26.658Z
+// auto-update 2025-09-08T02:33:31.482Z
