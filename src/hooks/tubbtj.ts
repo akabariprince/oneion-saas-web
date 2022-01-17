7f19d999a1c5304e6a19d04249babc631b4a8942
akabariprince <princeakabari123@gmail.com>
2022-01-17 18:54:00 +0530

    docs: add API usage example

--- src/hooks/tubbtj.ts
+++ src/hooks/tubbtj.ts
@@ -34,3 +34,4 @@ export const tubbtj = () => {
 // auto-update 2025-09-08T02:33:05.409Z
 // auto-update 2025-09-08T02:33:13.542Z
 // auto-update 2025-09-08T02:33:17.952Z
+// auto-update 2025-09-08T02:33:32.362Z
