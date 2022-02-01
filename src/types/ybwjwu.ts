ec089681eb613201b544b8959d43da5717ed2c00
akabariprince <princeakabari123@gmail.com>
2022-02-01 19:41:00 +0530

    fix: responsive design issue

--- src/types/ybwjwu.ts
+++ src/types/ybwjwu.ts
@@ -40,3 +40,4 @@ export const ybwjwu = () => {
 // auto-update 2025-09-08T02:33:23.673Z
 // auto-update 2025-09-08T02:33:26.858Z
 // auto-update 2025-09-08T02:33:31.805Z
+// auto-update 2025-09-08T02:33:34.304Z
