1e3cf2cfb2f771498cc31e0d47d43d604e35ebac
akabariprince <princeakabari123@gmail.com>
2022-02-08 10:06:00 +0530

    docs: add API usage example

--- src/components/tmhyak.tsx
+++ src/components/tmhyak.tsx
@@ -19,3 +19,4 @@ export default Tmhyak;
 // auto-update 2025-09-08T02:32:45.751Z
 // auto-update 2025-09-08T02:32:59.039Z
 // auto-update 2025-09-08T02:33:24.838Z
+// auto-update 2025-09-08T02:33:35.221Z
