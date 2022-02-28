5df8ab59aa72c9ecc1a48e0ec942de32bfedfbed
akabariprince <princeakabari123@gmail.com>
2022-02-28 14:35:00 +0530

    feat: integrate payment gateway

--- src/pages/cblbye.tsx
+++ src/pages/cblbye.tsx
@@ -26,3 +26,4 @@ export default Cblbye;
 // auto-update 2025-09-08T02:33:05.329Z
 // auto-update 2025-09-08T02:33:19.358Z
 // auto-update 2025-09-08T02:33:29.808Z
+// auto-update 2025-09-08T02:33:37.475Z
