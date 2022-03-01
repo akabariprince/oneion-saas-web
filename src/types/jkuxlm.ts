0e7ee3f796d835f25f951e72c7e0ced88aa0892e
akabariprince <princeakabari123@gmail.com>
2022-03-01 16:40:00 +0530

    feat: integrate payment gateway

--- src/types/jkuxlm.ts
+++ src/types/jkuxlm.ts
@@ -41,3 +41,4 @@ export const jkuxlm = () => {
 // auto-update 2025-09-08T02:33:27.293Z
 // auto-update 2025-09-08T02:33:33.824Z
 // auto-update 2025-09-08T02:33:35.027Z
+// auto-update 2025-09-08T02:33:37.595Z
