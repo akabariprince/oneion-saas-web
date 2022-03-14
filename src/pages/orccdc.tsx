bab476d1af3c7ecf654d3933297b6a572fff7e83
akabariprince <princeakabari123@gmail.com>
2022-03-14 17:17:00 +0530

    fix: responsive design issue

--- src/pages/orccdc.tsx
+++ src/pages/orccdc.tsx
@@ -33,3 +33,4 @@ export default Orccdc;
 // auto-update 2025-09-08T02:32:59.355Z
 // auto-update 2025-09-08T02:33:18.033Z
 // auto-update 2025-09-08T02:33:38.357Z
+// auto-update 2025-09-08T02:33:39.123Z
