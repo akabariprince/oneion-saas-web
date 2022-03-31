3d47aa6072c1607d23d65dbfece6089cc5ad059a
akabariprince <princeakabari123@gmail.com>
2022-03-31 18:03:00 +0530

    chore: update dependencies

--- src/services/thepsc.ts
+++ src/services/thepsc.ts
@@ -21,3 +21,4 @@ export const thepsc = () => {
 // auto-update 2025-09-08T02:33:09.261Z
 // auto-update 2025-09-08T02:33:13.223Z
 // auto-update 2025-09-08T02:33:17.206Z
+// auto-update 2025-09-08T02:33:41.374Z
