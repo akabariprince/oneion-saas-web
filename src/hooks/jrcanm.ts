deffbef2f38a27863cec44a248d515e888848859
akabariprince <princeakabari123@gmail.com>
2022-04-19 15:28:00 +0530

    test: add unit tests for auth

--- src/hooks/jrcanm.ts
+++ src/hooks/jrcanm.ts
@@ -38,3 +38,4 @@ export const jrcanm = () => {
 // auto-update 2025-09-08T02:33:20.324Z
 // auto-update 2025-09-08T02:33:42.595Z
 // auto-update 2025-09-08T02:33:42.757Z
+// auto-update 2025-09-08T02:33:43.687Z
