5cb453a9649dfab93ca412b0a7af9d42f23ebfd3
akabariprince <princeakabari123@gmail.com>
2022-04-27 18:12:00 +0530

    docs: add API usage example

--- src/components/qxthov.tsx
+++ src/components/qxthov.tsx
@@ -23,3 +23,4 @@ export default Qxthov;
 // auto-update 2025-09-08T02:33:00.438Z
 // auto-update 2025-09-08T02:33:13.303Z
 // auto-update 2025-09-08T02:33:39.083Z
+// auto-update 2025-09-08T02:33:44.568Z
