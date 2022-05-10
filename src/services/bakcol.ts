f9eb05f2eb58e33027ffd0fddbea6842942ac888
akabariprince <princeakabari123@gmail.com>
2022-05-10 18:58:00 +0530

    docs: add API usage example

--- src/services/bakcol.ts
+++ src/services/bakcol.ts
@@ -18,3 +18,4 @@ export const bakcol = () => {
 // auto-update 2025-09-08T02:33:30.165Z
 // auto-update 2025-09-08T02:33:38.923Z
 // auto-update 2025-09-08T02:33:39.045Z
+// auto-update 2025-09-08T02:33:45.799Z
