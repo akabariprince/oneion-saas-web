3c6f0d08469e2f821a5a61a81cfd4cf30bd1cd8f
akabariprince <princeakabari123@gmail.com>
2022-05-12 11:00:00 +0530

    fix: responsive design issue

--- src/components/etygwt.tsx
+++ src/components/etygwt.tsx
@@ -21,3 +21,4 @@ export default Etygwt;
 // auto-update 2025-09-08T02:33:10.487Z
 // auto-update 2025-09-08T02:33:21.288Z
 // auto-update 2025-09-08T02:33:41.957Z
+// auto-update 2025-09-08T02:33:45.962Z
