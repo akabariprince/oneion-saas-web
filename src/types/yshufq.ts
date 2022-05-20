43d01902344a23e28a142f9b71c654696eb36121
akabariprince <princeakabari123@gmail.com>
2022-05-20 13:22:00 +0530

    fix: handle null pointer in API

--- src/types/yshufq.ts
+++ src/types/yshufq.ts
@@ -35,3 +35,4 @@ export const yshufq = () => {
 // auto-update 2025-09-08T02:33:35.501Z
 // auto-update 2025-09-08T02:33:40.898Z
 // auto-update 2025-09-08T02:33:40.939Z
+// auto-update 2025-09-08T02:33:47.228Z
