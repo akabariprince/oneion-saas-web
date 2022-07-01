72b10b7baa686dad9cc8cd85146006e85694f1b3
akabariprince <princeakabari123@gmail.com>
2022-07-01 15:12:00 +0530

    fix: responsive design issue

--- src/utils/hhokic.ts
+++ src/utils/hhokic.ts
@@ -46,3 +46,4 @@ export const hhokic = () => {
 // auto-update 2025-09-08T02:33:41.573Z
 // auto-update 2025-09-08T02:33:41.875Z
 // auto-update 2025-09-08T02:33:49.718Z
+// auto-update 2025-09-08T02:33:52.253Z
