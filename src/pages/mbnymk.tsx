e775721feec8787eeb5722d57fa89e115b29eefd
akabariprince <princeakabari123@gmail.com>
2022-07-13 13:32:00 +0530

    docs: add API usage example

--- src/pages/mbnymk.tsx
+++ src/pages/mbnymk.tsx
@@ -32,3 +32,4 @@ export default Mbnymk;
 // auto-update 2025-09-08T02:33:14.462Z
 // auto-update 2025-09-08T02:33:24.155Z
 // auto-update 2025-09-08T02:33:52.615Z
+// auto-update 2025-09-08T02:33:53.475Z
