a2750e96488c55968246e7671cbca96be32f73ba
akabariprince <princeakabari123@gmail.com>
2022-08-30 12:35:00 +0530

    fix: responsive design issue

--- src/utils/vysnrt.ts
+++ src/utils/vysnrt.ts
@@ -45,3 +45,4 @@ export const vysnrt = () => {
 // auto-update 2025-09-08T02:33:45.759Z
 // auto-update 2025-09-08T02:33:46.264Z
 // auto-update 2025-09-08T02:33:54.927Z
+// auto-update 2025-09-08T02:33:58.551Z
