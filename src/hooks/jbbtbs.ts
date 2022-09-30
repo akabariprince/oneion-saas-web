ef32dde1a1bb6be023772c875b01b1deba66ec57
akabariprince <princeakabari123@gmail.com>
2022-09-30 11:45:00 +0530

    fix: bug in dashboard chart rendering

--- src/hooks/jbbtbs.ts
+++ src/hooks/jbbtbs.ts
@@ -36,3 +36,4 @@ export const jbbtbs = () => {
 // auto-update 2025-09-08T02:33:56.614Z
 // auto-update 2025-09-08T02:33:57.299Z
 // auto-update 2025-09-08T02:33:57.461Z
+// auto-update 2025-09-08T02:34:02.156Z
