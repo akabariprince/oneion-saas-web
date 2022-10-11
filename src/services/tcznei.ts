a9bf6606adeac1d5ec8837f9583dd206cee5aec8
akabariprince <princeakabari123@gmail.com>
2022-10-11 16:49:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/tcznei.ts
+++ src/services/tcznei.ts
@@ -33,3 +33,4 @@ export const tcznei = () => {
 // auto-update 2025-09-08T02:33:41.294Z
 // auto-update 2025-09-08T02:33:42.354Z
 // auto-update 2025-09-08T02:34:02.679Z
+// auto-update 2025-09-08T02:34:03.045Z
