ca96f16f959e816b8a2f0418ff8720ff23f21df1
akabariprince <princeakabari123@gmail.com>
2022-10-11 16:58:00 +0530

    docs: add API usage example

--- src/pages/cdbfys.tsx
+++ src/pages/cdbfys.tsx
@@ -32,3 +32,4 @@ export default Cdbfys;
 // auto-update 2025-09-08T02:33:34.743Z
 // auto-update 2025-09-08T02:33:47.148Z
 // auto-update 2025-09-08T02:33:59.621Z
+// auto-update 2025-09-08T02:34:03.202Z
