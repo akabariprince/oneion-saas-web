f07795db0461f8de1404084ac95f5d6a56965bc2
akabariprince <princeakabari123@gmail.com>
2022-10-12 18:03:00 +0530

    fix: handle null pointer in API

--- src/components/csoebg.tsx
+++ src/components/csoebg.tsx
@@ -30,3 +30,4 @@ export default Csoebg;
 // auto-update 2025-09-08T02:33:14.422Z
 // auto-update 2025-09-08T02:33:34.702Z
 // auto-update 2025-09-08T02:33:44.447Z
+// auto-update 2025-09-08T02:34:03.361Z
