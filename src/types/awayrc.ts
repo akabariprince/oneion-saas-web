eebbf14929c41e79e360df3ca6bc1ed560b94c84
akabariprince <princeakabari123@gmail.com>
2022-10-13 09:10:00 +0530

    fix: handle null pointer in API

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -40,3 +40,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:33:59.237Z
 // auto-update 2025-09-08T02:33:59.458Z
 // auto-update 2025-09-08T02:33:59.783Z
+// auto-update 2025-09-08T02:34:03.646Z
