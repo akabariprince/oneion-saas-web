4b37692bba21d0c235630d0d51758fc569a53813
akabariprince <princeakabari123@gmail.com>
2022-10-24 12:32:00 +0530

    chore: update dependencies

--- src/pages/mbnymk.tsx
+++ src/pages/mbnymk.tsx
@@ -36,3 +36,4 @@ export default Mbnymk;
 // auto-update 2025-09-08T02:33:57.421Z
 // auto-update 2025-09-08T02:34:02.760Z
 // auto-update 2025-09-08T02:34:03.443Z
+// auto-update 2025-09-08T02:34:04.820Z
