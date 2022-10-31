abde79dff9647e348554846a227abeb24e67b988
akabariprince <princeakabari123@gmail.com>
2022-10-31 09:51:00 +0530

    fix: responsive design issue

--- src/hooks/tubbtj.ts
+++ src/hooks/tubbtj.ts
@@ -46,3 +46,4 @@ export const tubbtj = () => {
 // auto-update 2025-09-08T02:33:59.158Z
 // auto-update 2025-09-08T02:34:00.627Z
 // auto-update 2025-09-08T02:34:03.729Z
+// auto-update 2025-09-08T02:34:05.543Z
