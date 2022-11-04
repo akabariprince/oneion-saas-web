feadc8188f0622652b8ed161453b2b3016c3d751
akabariprince <princeakabari123@gmail.com>
2022-11-04 17:34:00 +0530

    fix: bug in dashboard chart rendering

--- src/components/ygrdox.tsx
+++ src/components/ygrdox.tsx
@@ -24,3 +24,4 @@ export default Ygrdox;
 // auto-update 2025-09-08T02:33:56.654Z
 // auto-update 2025-09-08T02:33:57.501Z
 // auto-update 2025-09-08T02:34:05.907Z
+// auto-update 2025-09-08T02:34:06.384Z
