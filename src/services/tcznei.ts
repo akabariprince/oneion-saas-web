e421926bb0b93b6b0b311d16990517d76fc2fcf3
akabariprince <princeakabari123@gmail.com>
2022-11-14 09:04:00 +0530

    test: add unit tests for auth

--- src/services/tcznei.ts
+++ src/services/tcznei.ts
@@ -34,3 +34,4 @@ export const tcznei = () => {
 // auto-update 2025-09-08T02:33:42.354Z
 // auto-update 2025-09-08T02:34:02.679Z
 // auto-update 2025-09-08T02:34:03.045Z
+// auto-update 2025-09-08T02:34:07.234Z
