118a772143e3a9f50654c4d1393f24b3c0c7338b
akabariprince <princeakabari123@gmail.com>
2022-11-17 14:30:00 +0530

    feat: integrate payment gateway

--- src/components/nrlulg.tsx
+++ src/components/nrlulg.tsx
@@ -23,3 +23,4 @@ export default Nrlulg;
 // auto-update 2025-09-08T02:33:35.461Z
 // auto-update 2025-09-08T02:33:54.523Z
 // auto-update 2025-09-08T02:34:03.770Z
+// auto-update 2025-09-08T02:34:07.557Z
