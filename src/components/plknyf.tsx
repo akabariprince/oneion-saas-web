904ce99297ae3ed9f084f8bcb2874b4599e05edf
akabariprince <princeakabari123@gmail.com>
2022-11-18 13:02:00 +0530

    feat: integrate payment gateway

--- src/components/plknyf.tsx
+++ src/components/plknyf.tsx
@@ -20,3 +20,4 @@ export default Plknyf;
 // auto-update 2025-09-08T02:33:40.412Z
 // auto-update 2025-09-08T02:33:50.282Z
 // auto-update 2025-09-08T02:34:02.078Z
+// auto-update 2025-09-08T02:34:07.720Z
