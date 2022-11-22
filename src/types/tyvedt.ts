a7ac2c61a38cf804bfb0b36747161e7f4488c85a
akabariprince <princeakabari123@gmail.com>
2022-11-22 19:11:00 +0530

    fix: responsive design issue

--- src/types/tyvedt.ts
+++ src/types/tyvedt.ts
@@ -58,3 +58,4 @@ export const tyvedt = () => {
 // auto-update 2025-09-08T02:33:47.030Z
 // auto-update 2025-09-08T02:33:53.799Z
 // auto-update 2025-09-08T02:34:02.395Z
+// auto-update 2025-09-08T02:34:08.086Z
