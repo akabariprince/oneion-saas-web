8f0be0a1395d2c8ba8ac32cca81596f814dca717
akabariprince <princeakabari123@gmail.com>
2022-11-28 13:33:00 +0530

    fix: handle null pointer in API

--- src/services/qfanwv.ts
+++ src/services/qfanwv.ts
@@ -19,3 +19,4 @@ export const qfanwv = () => {
 // auto-update 2025-09-08T02:33:38.119Z
 // auto-update 2025-09-08T02:33:50.689Z
 // auto-update 2025-09-08T02:33:55.447Z
+// auto-update 2025-09-08T02:34:08.982Z
