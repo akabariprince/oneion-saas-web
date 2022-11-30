007822ee1e1c7099a65abf1beb932934758dbbaf
akabariprince <princeakabari123@gmail.com>
2022-11-30 16:29:00 +0530

    fix: bug in dashboard chart rendering

--- src/utils/ytptdu.ts
+++ src/utils/ytptdu.ts
@@ -42,3 +42,4 @@ export const ytptdu = () => {
 // auto-update 2025-09-08T02:34:00.953Z
 // auto-update 2025-09-08T02:34:03.605Z
 // auto-update 2025-09-08T02:34:04.535Z
+// auto-update 2025-09-08T02:34:09.378Z
