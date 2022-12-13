cd66d1328173b318ccfe72fdddcb33764ac7809f
akabariprince <princeakabari123@gmail.com>
2022-12-13 17:35:00 +0530

    docs: add API usage example

--- src/services/tcznei.ts
+++ src/services/tcznei.ts
@@ -35,3 +35,4 @@ export const tcznei = () => {
 // auto-update 2025-09-08T02:34:02.679Z
 // auto-update 2025-09-08T02:34:03.045Z
 // auto-update 2025-09-08T02:34:07.234Z
+// auto-update 2025-09-08T02:34:10.666Z
