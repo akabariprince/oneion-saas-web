49512b4a88d6bbaac6452417561ff7b514196c34
akabariprince <princeakabari123@gmail.com>
2022-12-13 18:16:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/hitzup.ts
+++ src/services/hitzup.ts
@@ -18,3 +18,4 @@ export const hitzup = () => {
 // auto-update 2025-09-08T02:33:41.254Z
 // auto-update 2025-09-08T02:33:49.760Z
 // auto-update 2025-09-08T02:33:51.013Z
+// auto-update 2025-09-08T02:34:10.870Z
