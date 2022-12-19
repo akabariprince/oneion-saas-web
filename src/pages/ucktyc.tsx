c3e77a39898a0b82d74dd07354793577f97f4afc
akabariprince <princeakabari123@gmail.com>
2022-12-19 19:54:00 +0530

    fix: handle null pointer in API

--- src/pages/ucktyc.tsx
+++ src/pages/ucktyc.tsx
@@ -35,3 +35,4 @@ export default Ucktyc;
 // auto-update 2025-09-08T02:33:55.164Z
 // auto-update 2025-09-08T02:33:57.868Z
 // auto-update 2025-09-08T02:34:07.476Z
+// auto-update 2025-09-08T02:34:11.513Z
