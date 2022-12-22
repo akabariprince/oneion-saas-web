88d2870c58d4cb9b5944f70112c0828923e5e9f4
akabariprince <princeakabari123@gmail.com>
2022-12-22 16:34:00 +0530

    fix: responsive design issue

--- src/components/sccewd.tsx
+++ src/components/sccewd.tsx
@@ -25,3 +25,4 @@ export default Sccewd;
 // auto-update 2025-09-08T02:34:05.261Z
 // auto-update 2025-09-08T02:34:08.164Z
 // auto-update 2025-09-08T02:34:10.544Z
+// auto-update 2025-09-08T02:34:11.962Z
