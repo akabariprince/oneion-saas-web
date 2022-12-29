fc204639e37910b6c0644f3b0e0d412120591096
akabariprince <princeakabari123@gmail.com>
2022-12-29 14:16:00 +0530

    docs: add API usage example

--- src/components/swcowh.tsx
+++ src/components/swcowh.tsx
@@ -28,3 +28,4 @@ export default Swcowh;
 // auto-update 2025-09-08T02:33:16.804Z
 // auto-update 2025-09-08T02:33:48.509Z
 // auto-update 2025-09-08T02:33:57.622Z
+// auto-update 2025-09-08T02:34:12.827Z
