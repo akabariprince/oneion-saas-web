73eaec365b65a6a362ff58bb645d0e70c1995619
akabariprince <princeakabari123@gmail.com>
2022-12-30 10:46:00 +0530

    feat: add AI prediction API

--- src/hooks/ankglw.ts
+++ src/hooks/ankglw.ts
@@ -45,3 +45,4 @@ export const ankglw = () => {
 // auto-update 2025-09-08T02:33:55.812Z
 // auto-update 2025-09-08T02:34:04.738Z
 // auto-update 2025-09-08T02:34:08.047Z
+// auto-update 2025-09-08T02:34:12.868Z
