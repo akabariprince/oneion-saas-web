4fcb984da72f564e60e3430695890f3f434b07f6
akabariprince <princeakabari123@gmail.com>
2022-12-30 15:46:00 +0530

    docs: add API usage example

--- src/components/rxkgus.tsx
+++ src/components/rxkgus.tsx
@@ -29,3 +29,4 @@ export default Rxkgus;
 // auto-update 2025-09-08T02:33:40.532Z
 // auto-update 2025-09-08T02:33:58.031Z
 // auto-update 2025-09-08T02:34:04.414Z
+// auto-update 2025-09-08T02:34:12.909Z
