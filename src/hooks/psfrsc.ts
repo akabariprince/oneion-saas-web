6a8abe675dfdd1b9a61e44181e8fab0cfd9af203
akabariprince <princeakabari123@gmail.com>
2023-01-03 14:33:00 +0530

    fix: responsive design issue

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -45,3 +45,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:33:53.595Z
 // auto-update 2025-09-08T02:34:00.708Z
 // auto-update 2025-09-08T02:34:11.716Z
+// auto-update 2025-09-08T02:34:13.269Z
