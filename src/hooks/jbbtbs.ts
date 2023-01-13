2d386cb582564c9224b933570d493ecd73f7ba90
akabariprince <princeakabari123@gmail.com>
2023-01-13 12:52:00 +0530

    chore: cleanup unused code

--- src/hooks/jbbtbs.ts
+++ src/hooks/jbbtbs.ts
@@ -38,3 +38,4 @@ export const jbbtbs = () => {
 // auto-update 2025-09-08T02:33:57.461Z
 // auto-update 2025-09-08T02:34:02.156Z
 // auto-update 2025-09-08T02:34:11.555Z
+// auto-update 2025-09-08T02:34:14.678Z
