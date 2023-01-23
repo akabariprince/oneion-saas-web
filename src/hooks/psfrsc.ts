b65386810a74f510fb354630ab869e66d62abe83
akabariprince <princeakabari123@gmail.com>
2023-01-23 12:10:00 +0530

    docs: update README

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -46,3 +46,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:34:00.708Z
 // auto-update 2025-09-08T02:34:11.716Z
 // auto-update 2025-09-08T02:34:13.269Z
+// auto-update 2025-09-08T02:34:15.763Z
