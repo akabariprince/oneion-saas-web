b600182cc15417fe768332ea31e62b5650f3d5a2
akabariprince <princeakabari123@gmail.com>
2023-02-07 16:10:00 +0530

    docs: add API usage example

--- src/components/gbsolk.tsx
+++ src/components/gbsolk.tsx
@@ -24,3 +24,4 @@ export default Gbsolk;
 // auto-update 2025-09-08T02:33:59.907Z
 // auto-update 2025-09-08T02:34:09.619Z
 // auto-update 2025-09-08T02:34:13.110Z
+// auto-update 2025-09-08T02:34:17.295Z
