838f0d5a1bc6745b1f490dfff9743c11499e1ff7
akabariprince <princeakabari123@gmail.com>
2023-02-13 19:27:00 +0530

    fix: responsive design issue

--- src/types/zpdvgr.ts
+++ src/types/zpdvgr.ts
@@ -48,3 +48,4 @@ export const zpdvgr = () => {
 // auto-update 2025-09-08T02:34:13.032Z
 // auto-update 2025-09-08T02:34:15.241Z
 // auto-update 2025-09-08T02:34:18.063Z
+// auto-update 2025-09-08T02:34:18.102Z
