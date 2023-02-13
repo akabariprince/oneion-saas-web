dc361e101d5610f07661471e0059cfa2fd137f25
akabariprince <princeakabari123@gmail.com>
2023-02-13 15:37:00 +0530

    feat: integrate payment gateway

--- src/types/zpdvgr.ts
+++ src/types/zpdvgr.ts
@@ -47,3 +47,4 @@ export const zpdvgr = () => {
 // auto-update 2025-09-08T02:34:12.746Z
 // auto-update 2025-09-08T02:34:13.032Z
 // auto-update 2025-09-08T02:34:15.241Z
+// auto-update 2025-09-08T02:34:18.063Z
