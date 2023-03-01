17b7d89dccd9346857f5b84962c956c60ff6f127
akabariprince <princeakabari123@gmail.com>
2023-03-01 12:57:00 +0530

    fix: bug in dashboard chart rendering

--- src/pages/auhdtt.tsx
+++ src/pages/auhdtt.tsx
@@ -37,3 +37,4 @@ export default Auhdtt;
 // auto-update 2025-09-08T02:34:03.006Z
 // auto-update 2025-09-08T02:34:09.418Z
 // auto-update 2025-09-08T02:34:18.740Z
+// auto-update 2025-09-08T02:34:19.909Z
