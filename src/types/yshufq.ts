8d4e85d357d2262a3cfc1a061c7649ef3aeaac1b
akabariprince <princeakabari123@gmail.com>
2023-03-06 11:41:00 +0530

    test: add unit tests for auth

--- src/types/yshufq.ts
+++ src/types/yshufq.ts
@@ -45,3 +45,4 @@ export const yshufq = () => {
 // auto-update 2025-09-08T02:34:15.004Z
 // auto-update 2025-09-08T02:34:16.244Z
 // auto-update 2025-09-08T02:34:19.624Z
+// auto-update 2025-09-08T02:34:20.753Z
