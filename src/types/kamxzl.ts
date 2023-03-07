d9709110a14969a0a5c4dab12f5ecc4f37a05fee
akabariprince <princeakabari123@gmail.com>
2023-03-07 16:12:00 +0530

    docs: add API usage example

--- src/types/kamxzl.ts
+++ src/types/kamxzl.ts
@@ -42,3 +42,4 @@ export const kamxzl = () => {
 // auto-update 2025-09-08T02:33:49.235Z
 // auto-update 2025-09-08T02:34:10.263Z
 // auto-update 2025-09-08T02:34:14.234Z
+// auto-update 2025-09-08T02:34:20.834Z
