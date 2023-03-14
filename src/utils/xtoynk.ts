b1e02482c1b430d26c48199645187755b0f708d7
akabariprince <princeakabari123@gmail.com>
2023-03-14 17:41:00 +0530

    chore: cleanup unused code

--- src/utils/xtoynk.ts
+++ src/utils/xtoynk.ts
@@ -39,3 +39,4 @@ export const xtoynk = () => {
 // auto-update 2025-09-08T02:34:12.085Z
 // auto-update 2025-09-08T02:34:20.672Z
 // auto-update 2025-09-08T02:34:21.196Z
+// auto-update 2025-09-08T02:34:21.840Z
