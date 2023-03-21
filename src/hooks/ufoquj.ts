067a6539dc484b6137bf933d0ec24dba229dc85d
akabariprince <princeakabari123@gmail.com>
2023-03-21 19:00:00 +0530

    fix: responsive design issue

--- src/hooks/ufoquj.ts
+++ src/hooks/ufoquj.ts
@@ -39,3 +39,4 @@ export const ufoquj = () => {
 // auto-update 2025-09-08T02:34:07.597Z
 // auto-update 2025-09-08T02:34:07.842Z
 // auto-update 2025-09-08T02:34:18.219Z
+// auto-update 2025-09-08T02:34:22.444Z
