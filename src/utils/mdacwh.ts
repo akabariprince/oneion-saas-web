676251be054a955f051bfc6a0c1cb5d13e6556ed
akabariprince <princeakabari123@gmail.com>
2023-03-23 16:14:00 +0530

    docs: add API usage example

--- src/utils/mdacwh.ts
+++ src/utils/mdacwh.ts
@@ -43,3 +43,4 @@ export const mdacwh = () => {
 // auto-update 2025-09-08T02:34:03.163Z
 // auto-update 2025-09-08T02:34:09.062Z
 // auto-update 2025-09-08T02:34:12.582Z
+// auto-update 2025-09-08T02:34:22.813Z
