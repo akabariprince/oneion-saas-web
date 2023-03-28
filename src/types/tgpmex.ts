3b3f1efb552e20d64ffdceaa664a6f38ca516492
akabariprince <princeakabari123@gmail.com>
2023-03-28 10:53:00 +0530

    feat: integrate payment gateway

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -54,3 +54,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:34:12.623Z
 // auto-update 2025-09-08T02:34:14.353Z
 // auto-update 2025-09-08T02:34:15.804Z
+// auto-update 2025-09-08T02:34:23.464Z
