845d2761f7ce84feca114fb6c0f45d10d7cd3b39
akabariprince <princeakabari123@gmail.com>
2023-03-28 17:22:00 +0530

    fix: handle null pointer in API

--- src/components/ijxexn.tsx
+++ src/components/ijxexn.tsx
@@ -25,3 +25,4 @@ export default Ijxexn;
 // auto-update 2025-09-08T02:34:09.498Z
 // auto-update 2025-09-08T02:34:17.215Z
 // auto-update 2025-09-08T02:34:20.550Z
+// auto-update 2025-09-08T02:34:23.505Z
