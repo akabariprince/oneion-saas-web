cd03728be52eb3d11e6fedb666c5c49e64990e0a
akabariprince <princeakabari123@gmail.com>
2023-04-25 10:05:00 +0530

    feat: implement login page

--- src/utils/mqaals.ts
+++ src/utils/mqaals.ts
@@ -33,3 +33,4 @@ export const mqaals = () => {
 // auto-update 2025-09-08T02:34:07.195Z
 // auto-update 2025-09-08T02:34:18.619Z
 // auto-update 2025-09-08T02:34:20.228Z
+// auto-update 2025-09-08T02:34:26.792Z
