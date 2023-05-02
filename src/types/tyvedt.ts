e7293bbad7dbb882dffc6db82cfeaa56982404d4
akabariprince <princeakabari123@gmail.com>
2023-05-02 16:19:00 +0530

    feat: integrate payment gateway

--- src/types/tyvedt.ts
+++ src/types/tyvedt.ts
@@ -61,3 +61,4 @@ export const tyvedt = () => {
 // auto-update 2025-09-08T02:34:08.086Z
 // auto-update 2025-09-08T02:34:10.067Z
 // auto-update 2025-09-08T02:34:11.352Z
+// auto-update 2025-09-08T02:34:27.432Z
