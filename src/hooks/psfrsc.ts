b20e9b7c12a1573dc705ec2769afb8c7666974f3
akabariprince <princeakabari123@gmail.com>
2023-05-04 19:33:00 +0530

    style: improve UI consistency

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -49,3 +49,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:34:15.763Z
 // auto-update 2025-09-08T02:34:26.354Z
 // auto-update 2025-09-08T02:34:27.392Z
+// auto-update 2025-09-08T02:34:27.953Z
