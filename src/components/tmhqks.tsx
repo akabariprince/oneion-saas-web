436d873798cb79aae518cc34b0a1092989f9fce0
akabariprince <princeakabari123@gmail.com>
2023-05-05 10:11:00 +0530

    fix: handle null pointer in API

--- src/components/tmhqks.tsx
+++ src/components/tmhqks.tsx
@@ -31,3 +31,4 @@ export default Tmhqks;
 // auto-update 2025-09-08T02:34:11.921Z
 // auto-update 2025-09-08T02:34:14.434Z
 // auto-update 2025-09-08T02:34:20.429Z
+// auto-update 2025-09-08T02:34:28.189Z
