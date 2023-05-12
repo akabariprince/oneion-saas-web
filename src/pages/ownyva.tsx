883ff5e92e09f102ece4007aaa8d55cf4f5ddd88
akabariprince <princeakabari123@gmail.com>
2023-05-12 15:42:00 +0530

    fix: bug in dashboard chart rendering

--- src/pages/ownyva.tsx
+++ src/pages/ownyva.tsx
@@ -42,3 +42,4 @@ export default Ownyva;
 // auto-update 2025-09-08T02:34:03.483Z
 // auto-update 2025-09-08T02:34:05.422Z
 // auto-update 2025-09-08T02:34:06.830Z
+// auto-update 2025-09-08T02:34:29.105Z
