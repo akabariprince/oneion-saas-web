d7b4e1f264d2a4384717d26d31a46e315060567b
akabariprince <princeakabari123@gmail.com>
2023-05-26 13:42:00 +0530

    feat: integrate payment gateway

--- src/components/sykqtv.tsx
+++ src/components/sykqtv.tsx
@@ -24,3 +24,4 @@ export default Sykqtv;
 // auto-update 2025-09-08T02:33:50.523Z
 // auto-update 2025-09-08T02:34:00.870Z
 // auto-update 2025-09-08T02:34:00.912Z
+// auto-update 2025-09-08T02:34:30.501Z
