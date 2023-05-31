e5dc7a6d8b5573b93ec7a9dc4642ab94c0c1b13d
akabariprince <princeakabari123@gmail.com>
2023-05-31 09:42:00 +0530

    fix: handle null pointer in API

--- src/utils/iujpcg.ts
+++ src/utils/iujpcg.ts
@@ -41,3 +41,4 @@ export const iujpcg = () => {
 // auto-update 2025-09-08T02:34:15.280Z
 // auto-update 2025-09-08T02:34:20.348Z
 // auto-update 2025-09-08T02:34:27.993Z
+// auto-update 2025-09-08T02:34:31.104Z
