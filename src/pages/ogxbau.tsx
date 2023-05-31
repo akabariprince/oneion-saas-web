53d4e39f6aed3e99b2ff411c5a33ec229b8eb5a0
akabariprince <princeakabari123@gmail.com>
2023-05-31 16:09:00 +0530

    fix: responsive design issue

--- src/pages/ogxbau.tsx
+++ src/pages/ogxbau.tsx
@@ -35,3 +35,4 @@ export default Ogxbau;
 // auto-update 2025-09-08T02:34:03.853Z
 // auto-update 2025-09-08T02:34:25.236Z
 // auto-update 2025-09-08T02:34:27.471Z
+// auto-update 2025-09-08T02:34:31.181Z
