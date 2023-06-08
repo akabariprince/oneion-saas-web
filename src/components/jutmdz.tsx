dfb9dd5606b68c2eeb2b68b799caecd78991229f
akabariprince <princeakabari123@gmail.com>
2023-06-08 11:43:00 +0530

    fix: handle null pointer in API

--- src/components/jutmdz.tsx
+++ src/components/jutmdz.tsx
@@ -31,3 +31,4 @@ export default Jutmdz;
 // auto-update 2025-09-08T02:34:12.124Z
 // auto-update 2025-09-08T02:34:13.672Z
 // auto-update 2025-09-08T02:34:31.300Z
+// auto-update 2025-09-08T02:34:32.303Z
