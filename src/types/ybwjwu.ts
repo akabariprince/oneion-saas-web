8e6d007ff5b7a771959f3fc4034283a4bcd1acf4
akabariprince <princeakabari123@gmail.com>
2023-06-19 09:28:00 +0530

    feat: integrate payment gateway

--- src/types/ybwjwu.ts
+++ src/types/ybwjwu.ts
@@ -50,3 +50,4 @@ export const ybwjwu = () => {
 // auto-update 2025-09-08T02:33:59.079Z
 // auto-update 2025-09-08T02:34:20.031Z
 // auto-update 2025-09-08T02:34:22.484Z
+// auto-update 2025-09-08T02:34:33.263Z
