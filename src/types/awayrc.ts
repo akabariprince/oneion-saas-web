411905997badce4bda5d4a5129991ce031e92bad
akabariprince <princeakabari123@gmail.com>
2023-07-26 14:42:00 +0530

    style: improve UI consistency

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -46,3 +46,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:34:18.024Z
 // auto-update 2025-09-08T02:34:33.145Z
 // auto-update 2025-09-08T02:34:36.428Z
+// auto-update 2025-09-08T02:34:37.311Z
