7881da684ea2043461b9fe26b0b917dc6183af9b
akabariprince <princeakabari123@gmail.com>
2023-08-03 15:52:00 +0530

    fix: bug in dashboard chart rendering

--- src/components/tmhyak.tsx
+++ src/components/tmhyak.tsx
@@ -22,3 +22,4 @@ export default Tmhyak;
 // auto-update 2025-09-08T02:33:35.221Z
 // auto-update 2025-09-08T02:33:46.002Z
 // auto-update 2025-09-08T02:33:51.370Z
+// auto-update 2025-09-08T02:34:38.113Z
