58318457d17224d262ca85a87122f73b6196d5a1
akabariprince <princeakabari123@gmail.com>
2023-08-22 13:05:00 +0530

    docs: add API usage example

--- src/components/avszyr.tsx
+++ src/components/avszyr.tsx
@@ -25,3 +25,4 @@ export default Avszyr;
 // auto-update 2025-09-08T02:34:20.071Z
 // auto-update 2025-09-08T02:34:24.389Z
 // auto-update 2025-09-08T02:34:31.903Z
+// auto-update 2025-09-08T02:34:40.290Z
