0266984cfaea35f1ea5437a63b4bebc9db51d374
akabariprince <princeakabari123@gmail.com>
2023-08-23 13:47:00 +0530

    fix: handle null pointer in API

--- src/types/kpkyfh.ts
+++ src/types/kpkyfh.ts
@@ -54,3 +54,4 @@ export const kpkyfh = () => {
 // auto-update 2025-09-08T02:34:36.588Z
 // auto-update 2025-09-08T02:34:37.352Z
 // auto-update 2025-09-08T02:34:38.393Z
+// auto-update 2025-09-08T02:34:40.734Z
