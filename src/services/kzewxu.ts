5bed8608f9114e180971bbb06d7bff31a947a0de
akabariprince <princeakabari123@gmail.com>
2023-08-30 13:25:00 +0530

    fix: responsive design issue

--- src/services/kzewxu.ts
+++ src/services/kzewxu.ts
@@ -36,3 +36,4 @@ export const kzewxu = () => {
 // auto-update 2025-09-08T02:34:10.106Z
 // auto-update 2025-09-08T02:34:14.557Z
 // auto-update 2025-09-08T02:34:24.915Z
+// auto-update 2025-09-08T02:34:41.269Z
