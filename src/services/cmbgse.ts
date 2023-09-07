b0b76130aa3918f3dd60ad5d621d6d66f8a9f794
akabariprince <princeakabari123@gmail.com>
2023-09-07 18:58:00 +0530

    docs: add API usage example

--- src/services/cmbgse.ts
+++ src/services/cmbgse.ts
@@ -38,3 +38,4 @@ export const cmbgse = () => {
 // auto-update 2025-09-08T02:34:21.637Z
 // auto-update 2025-09-08T02:34:25.617Z
 // auto-update 2025-09-08T02:34:41.650Z
+// auto-update 2025-09-08T02:34:42.500Z
