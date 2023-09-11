61cf657f20c61d9023f6af81b56e5c035d8fb1c3
akabariprince <princeakabari123@gmail.com>
2023-09-11 12:52:00 +0530

    fix: responsive design issue

--- src/hooks/igwaqz.ts
+++ src/hooks/igwaqz.ts
@@ -56,3 +56,4 @@ export const igwaqz = () => {
 // auto-update 2025-09-08T02:34:23.911Z
 // auto-update 2025-09-08T02:34:27.590Z
 // auto-update 2025-09-08T02:34:32.461Z
+// auto-update 2025-09-08T02:34:42.824Z
