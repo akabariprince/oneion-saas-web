976c5066c4c912cf91682f70d77a7e8c1ab178db
akabariprince <princeakabari123@gmail.com>
2023-09-19 10:49:00 +0530

    fix: bug in dashboard chart rendering

--- src/components/avszyr.tsx
+++ src/components/avszyr.tsx
@@ -27,3 +27,4 @@ export default Avszyr;
 // auto-update 2025-09-08T02:34:31.903Z
 // auto-update 2025-09-08T02:34:40.290Z
 // auto-update 2025-09-08T02:34:43.106Z
+// auto-update 2025-09-08T02:34:43.865Z
