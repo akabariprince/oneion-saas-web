2c8ccdc477618d6943bf4ecedd95f096b8d2cc97
akabariprince <princeakabari123@gmail.com>
2023-09-28 11:43:00 +0530

    docs: add API usage example

--- src/components/djeebq.tsx
+++ src/components/djeebq.tsx
@@ -21,3 +21,4 @@ export default Djeebq;
 // auto-update 2025-09-08T02:34:19.104Z
 // auto-update 2025-09-08T02:34:29.904Z
 // auto-update 2025-09-08T02:34:32.343Z
+// auto-update 2025-09-08T02:34:44.995Z
