3f9da30513f09411978882196f430c0c1e71ff42
akabariprince <princeakabari123@gmail.com>
2023-09-29 17:40:00 +0530

    docs: add API usage example

--- src/pages/pjdmst.tsx
+++ src/pages/pjdmst.tsx
@@ -43,3 +43,4 @@ export default Pjdmst;
 // auto-update 2025-09-08T02:34:23.143Z
 // auto-update 2025-09-08T02:34:31.026Z
 // auto-update 2025-09-08T02:34:39.277Z
+// auto-update 2025-09-08T02:34:45.232Z
