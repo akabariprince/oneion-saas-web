efdf89934fea88c9861b08e649ae79cdbbf370a5
akabariprince <princeakabari123@gmail.com>
2023-10-02 15:00:00 +0530

    fix: handle null pointer in API

--- src/utils/ytptdu.ts
+++ src/utils/ytptdu.ts
@@ -51,3 +51,4 @@ export const ytptdu = () => {
 // auto-update 2025-09-08T02:34:22.323Z
 // auto-update 2025-09-08T02:34:32.702Z
 // auto-update 2025-09-08T02:34:40.911Z
+// auto-update 2025-09-08T02:34:45.311Z
