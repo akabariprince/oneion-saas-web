4db7fe28f974fc030516bc63efcf15f0f808e353
akabariprince <princeakabari123@gmail.com>
2023-10-11 13:25:00 +0530

    fix: handle null pointer in API

--- src/services/qssmyk.ts
+++ src/services/qssmyk.ts
@@ -27,3 +27,4 @@ export const qssmyk = () => {
 // auto-update 2025-09-08T02:34:12.664Z
 // auto-update 2025-09-08T02:34:20.590Z
 // auto-update 2025-09-08T02:34:34.507Z
+// auto-update 2025-09-08T02:34:46.196Z
