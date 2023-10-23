62a4abaaf6f6b9f958d1e272ef82290c7d5396f8
akabariprince <princeakabari123@gmail.com>
2023-10-23 10:45:00 +0530

    refactor: optimize API service

--- src/hooks/jbbtbs.ts
+++ src/hooks/jbbtbs.ts
@@ -46,3 +46,4 @@ export const jbbtbs = () => {
 // auto-update 2025-09-08T02:34:34.348Z
 // auto-update 2025-09-08T02:34:44.265Z
 // auto-update 2025-09-08T02:34:44.872Z
+// auto-update 2025-09-08T02:34:47.648Z
