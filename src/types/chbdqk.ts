12fad0161fa3c0ef23764f9f704f865b06328e06
akabariprince <princeakabari123@gmail.com>
2023-10-24 15:37:00 +0530

    fix: bug in dashboard chart rendering

--- src/types/chbdqk.ts
+++ src/types/chbdqk.ts
@@ -53,3 +53,4 @@ export const chbdqk = () => {
 // auto-update 2025-09-08T02:34:37.471Z
 // auto-update 2025-09-08T02:34:37.791Z
 // auto-update 2025-09-08T02:34:44.955Z
+// auto-update 2025-09-08T02:34:47.933Z
