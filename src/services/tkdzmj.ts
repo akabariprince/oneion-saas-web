7af8f6edc8a6b961d03ed3eee00bdea9f222e222
akabariprince <princeakabari123@gmail.com>
2023-10-27 12:31:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/tkdzmj.ts
+++ src/services/tkdzmj.ts
@@ -32,3 +32,4 @@ export const tkdzmj = () => {
 // auto-update 2025-09-08T02:34:42.540Z
 // auto-update 2025-09-08T02:34:44.588Z
 // auto-update 2025-09-08T02:34:45.114Z
+// auto-update 2025-09-08T02:34:48.453Z
