f99cfa51c921952e0d32f305c59759ed009a5ecd
akabariprince <princeakabari123@gmail.com>
2023-10-30 09:14:00 +0530

    fix: responsive design issue

--- src/components/plknyf.tsx
+++ src/components/plknyf.tsx
@@ -22,3 +22,4 @@ export default Plknyf;
 // auto-update 2025-09-08T02:34:02.078Z
 // auto-update 2025-09-08T02:34:07.720Z
 // auto-update 2025-09-08T02:34:21.038Z
+// auto-update 2025-09-08T02:34:48.820Z
