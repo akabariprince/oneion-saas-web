b33c90b4ce0e7ec9bbadde5e601db2642e59f1d4
akabariprince <princeakabari123@gmail.com>
2023-10-31 15:21:00 +0530

    fix: handle null pointer in API

--- src/hooks/jrcanm.ts
+++ src/hooks/jrcanm.ts
@@ -52,3 +52,4 @@ export const jrcanm = () => {
 // auto-update 2025-09-08T02:34:35.307Z
 // auto-update 2025-09-08T02:34:35.952Z
 // auto-update 2025-09-08T02:34:40.250Z
+// auto-update 2025-09-08T02:34:49.181Z
