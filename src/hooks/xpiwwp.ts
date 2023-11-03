ff50b1e1836d5ed9a863442f45dcf82593947846
akabariprince <princeakabari123@gmail.com>
2023-11-03 10:49:00 +0530

    docs: add API usage example

--- src/hooks/xpiwwp.ts
+++ src/hooks/xpiwwp.ts
@@ -52,3 +52,4 @@ export const xpiwwp = () => {
 // auto-update 2025-09-08T02:34:40.330Z
 // auto-update 2025-09-08T02:34:41.470Z
 // auto-update 2025-09-08T02:34:41.551Z
+// auto-update 2025-09-08T02:34:49.783Z
