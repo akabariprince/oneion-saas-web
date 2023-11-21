3ff9a3ddf978c2d76940bb3dd98e89e8e219eb45
akabariprince <princeakabari123@gmail.com>
2023-11-21 12:17:00 +0530

    test: improve coverage for utils

--- src/services/oljpot.ts
+++ src/services/oljpot.ts
@@ -24,3 +24,4 @@ export const oljpot = () => {
 // auto-update 2025-09-08T02:34:16.165Z
 // auto-update 2025-09-08T02:34:30.422Z
 // auto-update 2025-09-08T02:34:42.296Z
+// auto-update 2025-09-08T02:34:51.466Z
