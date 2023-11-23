f9c82a42a6505760a854a67b5aac891595fa0309
akabariprince <princeakabari123@gmail.com>
2023-11-23 12:58:00 +0530

    feat: add AI prediction API

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -58,3 +58,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:34:46.395Z
 // auto-update 2025-09-08T02:34:48.861Z
 // auto-update 2025-09-08T02:34:51.267Z
+// auto-update 2025-09-08T02:34:51.993Z
