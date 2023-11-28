f57ffb3b8a9e2aed2670f5bb9517a34bcd9f0e3b
akabariprince <princeakabari123@gmail.com>
2023-11-28 09:11:00 +0530

    fix: handle null pointer in API

--- src/components/avszyr.tsx
+++ src/components/avszyr.tsx
@@ -30,3 +30,4 @@ export default Avszyr;
 // auto-update 2025-09-08T02:34:43.865Z
 // auto-update 2025-09-08T02:34:44.427Z
 // auto-update 2025-09-08T02:34:48.535Z
+// auto-update 2025-09-08T02:34:52.193Z
