bde3f70f84873b82cd6e97c3c7517eab7b5ca08e
akabariprince <princeakabari123@gmail.com>
2023-11-30 11:33:00 +0530

    fix: responsive design issue

--- src/services/tkdzmj.ts
+++ src/services/tkdzmj.ts
@@ -33,3 +33,4 @@ export const tkdzmj = () => {
 // auto-update 2025-09-08T02:34:44.588Z
 // auto-update 2025-09-08T02:34:45.114Z
 // auto-update 2025-09-08T02:34:48.453Z
+// auto-update 2025-09-08T02:34:52.695Z
