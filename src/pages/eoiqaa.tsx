ddd6af7bddcc8b1101856a8e791fd4031260218f
akabariprince <princeakabari123@gmail.com>
2023-12-05 11:05:00 +0530

    refactor: optimize API service

--- src/pages/eoiqaa.tsx
+++ src/pages/eoiqaa.tsx
@@ -38,3 +38,4 @@ export default Eoiqaa;
 // auto-update 2025-09-08T02:34:38.593Z
 // auto-update 2025-09-08T02:34:49.142Z
 // auto-update 2025-09-08T02:34:49.380Z
+// auto-update 2025-09-08T02:34:53.180Z
