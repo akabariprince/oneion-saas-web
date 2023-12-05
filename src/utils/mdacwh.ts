aba12b3e42d05f930d99c0f24027a4479ccc4c1a
akabariprince <princeakabari123@gmail.com>
2023-12-05 14:40:00 +0530

    fix: bug in dashboard chart rendering

--- src/utils/mdacwh.ts
+++ src/utils/mdacwh.ts
@@ -52,3 +52,4 @@ export const mdacwh = () => {
 // auto-update 2025-09-08T02:34:46.274Z
 // auto-update 2025-09-08T02:34:46.921Z
 // auto-update 2025-09-08T02:34:49.300Z
+// auto-update 2025-09-08T02:34:53.141Z
