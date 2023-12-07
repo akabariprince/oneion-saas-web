783da7f4d30afc9046ecc380b106bc4c2b05a0e0
akabariprince <princeakabari123@gmail.com>
2023-12-07 11:31:00 +0530

    docs: add API usage example

--- src/hooks/psfrsc.ts
+++ src/hooks/psfrsc.ts
@@ -59,3 +59,4 @@ export const psfrsc = () => {
 // auto-update 2025-09-08T02:34:48.861Z
 // auto-update 2025-09-08T02:34:51.267Z
 // auto-update 2025-09-08T02:34:51.993Z
+// auto-update 2025-09-08T02:34:53.581Z
