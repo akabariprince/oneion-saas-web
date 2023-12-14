854359af95f36f346070b8ccd02f18f639e052d9
akabariprince <princeakabari123@gmail.com>
2023-12-14 10:42:00 +0530

    chore: update dependencies

--- src/utils/vysnrt.ts
+++ src/utils/vysnrt.ts
@@ -60,3 +60,4 @@ export const vysnrt = () => {
 // auto-update 2025-09-08T02:34:49.904Z
 // auto-update 2025-09-08T02:34:50.787Z
 // auto-update 2025-09-08T02:34:52.311Z
+// auto-update 2025-09-08T02:34:54.754Z
