708aa232e6f790738de8570180af9f6d632066f4
akabariprince <princeakabari123@gmail.com>
2023-12-14 13:03:00 +0530

    feat: add AI prediction API

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -62,3 +62,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:34:38.877Z
 // auto-update 2025-09-08T02:34:43.184Z
 // auto-update 2025-09-08T02:34:45.713Z
+// auto-update 2025-09-08T02:34:54.835Z
