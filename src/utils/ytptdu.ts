46fbd2d451b18e6293169faf93ede6fe59a8d62f
akabariprince <princeakabari123@gmail.com>
2023-12-18 17:17:00 +0530

    style: improve UI consistency

--- src/utils/ytptdu.ts
+++ src/utils/ytptdu.ts
@@ -53,3 +53,4 @@ export const ytptdu = () => {
 // auto-update 2025-09-08T02:34:40.911Z
 // auto-update 2025-09-08T02:34:45.311Z
 // auto-update 2025-09-08T02:34:49.824Z
+// auto-update 2025-09-08T02:34:54.997Z
