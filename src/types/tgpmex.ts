873d4dbe3390bd9cb19372763742a9675f467d68
akabariprince <princeakabari123@gmail.com>
2023-12-29 15:19:00 +0530

    docs: update README

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -63,3 +63,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:34:43.184Z
 // auto-update 2025-09-08T02:34:45.713Z
 // auto-update 2025-09-08T02:34:54.835Z
+// auto-update 2025-09-08T02:34:56.046Z
