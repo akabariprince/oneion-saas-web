088e142e5fa99321ea5fe617237352e5696921e6
akabariprince <princeakabari123@gmail.com>
2024-01-05 16:33:00 +0530

    feat: integrate payment gateway

--- src/services/ayalbx.ts
+++ src/services/ayalbx.ts
@@ -23,3 +23,4 @@ export const ayalbx = () => {
 // auto-update 2025-09-08T02:34:11.112Z
 // auto-update 2025-09-08T02:34:29.184Z
 // auto-update 2025-09-08T02:34:46.078Z
+// auto-update 2025-09-08T02:34:56.444Z
