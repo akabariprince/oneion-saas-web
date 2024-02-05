81e1891be3470a9239259ae8267506590c918c2b
akabariprince <princeakabari123@gmail.com>
2024-02-05 11:32:00 +0530

    chore: cleanup unused code

--- src/hooks/xpiwwp.ts
+++ src/hooks/xpiwwp.ts
@@ -58,3 +58,4 @@ export const xpiwwp = () => {
 // auto-update 2025-09-08T02:34:57.296Z
 // auto-update 2025-09-08T02:34:58.067Z
 // auto-update 2025-09-08T02:34:58.914Z
+// auto-update 2025-09-08T02:34:59.594Z
