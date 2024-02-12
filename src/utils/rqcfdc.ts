41a8a3d399e2ddbae611060a260021cead7ae0a8
akabariprince <princeakabari123@gmail.com>
2024-02-12 09:05:00 +0530

    style: improve UI consistency

--- src/utils/rqcfdc.ts
+++ src/utils/rqcfdc.ts
@@ -58,3 +58,4 @@ export const rqcfdc = () => {
 // auto-update 2025-09-08T02:34:51.346Z
 // auto-update 2025-09-08T02:34:52.114Z
 // auto-update 2025-09-08T02:34:56.125Z
+// auto-update 2025-09-08T02:35:00.240Z
