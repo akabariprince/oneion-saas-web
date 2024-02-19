944fa3a9d59505fd95d33b8654632faf53507420
akabariprince <princeakabari123@gmail.com>
2024-02-19 11:26:00 +0530

    fix: responsive design issue

--- src/components/phfupq.tsx
+++ src/components/phfupq.tsx
@@ -30,3 +30,4 @@ export default Phfupq;
 // auto-update 2025-09-08T02:34:54.108Z
 // auto-update 2025-09-08T02:34:59.076Z
 // auto-update 2025-09-08T02:34:59.635Z
+// auto-update 2025-09-08T02:35:00.930Z
