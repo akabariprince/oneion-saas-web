a2507f584912dcca7eaac67ea5aec545d936221a
akabariprince <princeakabari123@gmail.com>
2024-02-23 11:03:00 +0530

    fix: handle null pointer in API

--- src/utils/uemxlm.ts
+++ src/utils/uemxlm.ts
@@ -58,3 +58,4 @@ export const uemxlm = () => {
 // auto-update 2025-09-08T02:34:45.633Z
 // auto-update 2025-09-08T02:34:53.622Z
 // auto-update 2025-09-08T02:34:58.751Z
+// auto-update 2025-09-08T02:35:01.291Z
