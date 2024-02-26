b8970199653f89612bfabb2598be36ce6548af10
akabariprince <princeakabari123@gmail.com>
2024-02-26 17:21:00 +0530

    test: improve coverage for utils

--- src/services/ozbjzs.ts
+++ src/services/ozbjzs.ts
@@ -30,3 +30,4 @@ export const ozbjzs = () => {
 // auto-update 2025-09-08T02:34:42.580Z
 // auto-update 2025-09-08T02:34:48.943Z
 // auto-update 2025-09-08T02:34:54.187Z
+// auto-update 2025-09-08T02:35:01.371Z
