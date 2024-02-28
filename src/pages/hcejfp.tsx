6762ad2c92627a69d82fabdefe03ba03bc86bf6a
akabariprince <princeakabari123@gmail.com>
2024-02-28 19:14:00 +0530

    fix: responsive design issue

--- src/pages/hcejfp.tsx
+++ src/pages/hcejfp.tsx
@@ -49,3 +49,4 @@ export default Hcejfp;
 // auto-update 2025-09-08T02:34:43.784Z
 // auto-update 2025-09-08T02:35:00.001Z
 // auto-update 2025-09-08T02:35:01.131Z
+// auto-update 2025-09-08T02:35:01.779Z
