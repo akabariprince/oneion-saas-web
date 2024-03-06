ccd1ccad2a9ac4f87313e93e6f01e2de6278d09f
akabariprince <princeakabari123@gmail.com>
2024-03-06 09:21:00 +0530

    feat: integrate payment gateway

--- src/components/avszyr.tsx
+++ src/components/avszyr.tsx
@@ -31,3 +31,4 @@ export default Avszyr;
 // auto-update 2025-09-08T02:34:44.427Z
 // auto-update 2025-09-08T02:34:48.535Z
 // auto-update 2025-09-08T02:34:52.193Z
+// auto-update 2025-09-08T02:35:02.505Z
