e8ab426dbf721398d5b77f9bd1e4ae4237738d28
akabariprince <princeakabari123@gmail.com>
2024-03-07 15:34:00 +0530

    chore: cleanup unused code

--- src/services/hitzup.ts
+++ src/services/hitzup.ts
@@ -23,3 +23,4 @@ export const hitzup = () => {
 // auto-update 2025-09-08T02:34:46.638Z
 // auto-update 2025-09-08T02:34:56.729Z
 // auto-update 2025-09-08T02:35:00.563Z
+// auto-update 2025-09-08T02:35:02.873Z
