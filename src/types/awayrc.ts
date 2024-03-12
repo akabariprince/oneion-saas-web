6e56b9e43e12056a60f153d47743e7fc97c6c43c
akabariprince <princeakabari123@gmail.com>
2024-03-12 15:12:00 +0530

    feat: add AI prediction API

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -51,3 +51,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:34:47.486Z
 // auto-update 2025-09-08T02:34:56.975Z
 // auto-update 2025-09-08T02:34:59.796Z
+// auto-update 2025-09-08T02:35:03.352Z
