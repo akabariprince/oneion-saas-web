c8af44eccb4b2f24ef8b1a8208785fb303470010
akabariprince <princeakabari123@gmail.com>
2024-03-14 19:24:00 +0530

    style: improve UI consistency

--- src/pages/ownyva.tsx
+++ src/pages/ownyva.tsx
@@ -49,3 +49,4 @@ export default Ownyva;
 // auto-update 2025-09-08T02:34:40.131Z
 // auto-update 2025-09-08T02:34:41.813Z
 // auto-update 2025-09-08T02:34:49.460Z
+// auto-update 2025-09-08T02:35:03.718Z
