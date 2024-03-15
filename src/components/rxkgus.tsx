2f5a8fce05d326f3f004165b3682e260bf807a67
akabariprince <princeakabari123@gmail.com>
2024-03-15 14:06:00 +0530

    fix: handle null pointer in API

--- src/components/rxkgus.tsx
+++ src/components/rxkgus.tsx
@@ -35,3 +35,4 @@ export default Rxkgus;
 // auto-update 2025-09-08T02:34:41.189Z
 // auto-update 2025-09-08T02:34:42.137Z
 // auto-update 2025-09-08T02:35:02.914Z
+// auto-update 2025-09-08T02:35:03.799Z
