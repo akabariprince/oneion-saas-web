0cf7690682ff32a7c4807002cce36ec4bad1ee76
akabariprince <princeakabari123@gmail.com>
2024-03-15 09:43:00 +0530

    chore: update dependencies

--- src/types/thrsdg.ts
+++ src/types/thrsdg.ts
@@ -58,3 +58,4 @@ export const thrsdg = () => {
 // auto-update 2025-09-08T02:34:54.307Z
 // auto-update 2025-09-08T02:35:00.401Z
 // auto-update 2025-09-08T02:35:00.441Z
+// auto-update 2025-09-08T02:35:03.920Z
