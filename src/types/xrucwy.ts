137d4c2ce9d76fbbbfb029b501640646e3d600db
akabariprince <princeakabari123@gmail.com>
2024-03-21 13:09:00 +0530

    test: improve coverage for utils

--- src/types/xrucwy.ts
+++ src/types/xrucwy.ts
@@ -51,3 +51,4 @@ export const xrucwy = () => {
 // auto-update 2025-09-08T02:34:46.235Z
 // auto-update 2025-09-08T02:34:51.188Z
 // auto-update 2025-09-08T02:34:57.539Z
+// auto-update 2025-09-08T02:35:04.721Z
