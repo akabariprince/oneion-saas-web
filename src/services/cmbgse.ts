367d7ee776e2dd34efb6c1b7db84e7228963ea1a
akabariprince <princeakabari123@gmail.com>
2024-03-25 18:58:00 +0530

    test: add unit tests for auth

--- src/services/cmbgse.ts
+++ src/services/cmbgse.ts
@@ -43,3 +43,4 @@ export const cmbgse = () => {
 // auto-update 2025-09-08T02:34:54.428Z
 // auto-update 2025-09-08T02:34:57.417Z
 // auto-update 2025-09-08T02:35:04.282Z
+// auto-update 2025-09-08T02:35:05.124Z
