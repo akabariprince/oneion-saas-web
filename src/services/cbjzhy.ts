931ae9f0b78d445ea60e0f32e19da301799e7ae8
akabariprince <princeakabari123@gmail.com>
2024-04-03 19:41:00 +0530

    fix: handle null pointer in API

--- src/services/cbjzhy.ts
+++ src/services/cbjzhy.ts
@@ -33,3 +33,4 @@ export const cbjzhy = () => {
 // auto-update 2025-09-08T02:34:58.427Z
 // auto-update 2025-09-08T02:35:02.586Z
 // auto-update 2025-09-08T02:35:05.383Z
+// auto-update 2025-09-08T02:35:06.347Z
