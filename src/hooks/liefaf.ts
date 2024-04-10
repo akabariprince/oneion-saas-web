193c5ce5871b85f31e37741d275afc7b683be9c2
akabariprince <princeakabari123@gmail.com>
2024-04-10 18:28:00 +0530

    test: add unit tests for auth

--- src/hooks/liefaf.ts
+++ src/hooks/liefaf.ts
@@ -46,3 +46,4 @@ export const liefaf = () => {
 // auto-update 2025-09-08T02:34:46.117Z
 // auto-update 2025-09-08T02:34:47.770Z
 // auto-update 2025-09-08T02:34:50.990Z
+// auto-update 2025-09-08T02:35:07.278Z
