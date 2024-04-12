ae9ff829384a0af5dd81e1c53675098ef249c044
akabariprince <princeakabari123@gmail.com>
2024-04-12 11:24:00 +0530

    fix: handle null pointer in API

--- src/types/zpdvgr.ts
+++ src/types/zpdvgr.ts
@@ -60,3 +60,4 @@ export const zpdvgr = () => {
 // auto-update 2025-09-08T02:34:52.153Z
 // auto-update 2025-09-08T02:34:58.873Z
 // auto-update 2025-09-08T02:35:03.233Z
+// auto-update 2025-09-08T02:35:07.558Z
