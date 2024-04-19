bc884b2de9f5d3725f0d1d7954c6e1b6ad045b67
akabariprince <princeakabari123@gmail.com>
2024-04-19 16:17:00 +0530

    chore: update dependencies

--- src/hooks/jrcanm.ts
+++ src/hooks/jrcanm.ts
@@ -57,3 +57,4 @@ export const jrcanm = () => {
 // auto-update 2025-09-08T02:34:54.029Z
 // auto-update 2025-09-08T02:34:54.548Z
 // auto-update 2025-09-08T02:35:06.834Z
+// auto-update 2025-09-08T02:35:08.125Z
