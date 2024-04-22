9d1bf0a1e4d0c187568cba287384a96d60106d71
akabariprince <princeakabari123@gmail.com>
2024-04-22 19:56:00 +0530

    docs: add API usage example

--- src/utils/xtoynk.ts
+++ src/utils/xtoynk.ts
@@ -49,3 +49,4 @@ export const xtoynk = () => {
 // auto-update 2025-09-08T02:34:49.541Z
 // auto-update 2025-09-08T02:34:57.137Z
 // auto-update 2025-09-08T02:35:02.385Z
+// auto-update 2025-09-08T02:35:08.566Z
