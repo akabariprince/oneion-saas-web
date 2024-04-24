6305686e7c0e2a115916b64bde9ecf11861cd8de
akabariprince <princeakabari123@gmail.com>
2024-04-24 10:23:00 +0530

    refactor: optimize API service

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -64,3 +64,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:34:45.713Z
 // auto-update 2025-09-08T02:34:54.835Z
 // auto-update 2025-09-08T02:34:56.046Z
+// auto-update 2025-09-08T02:35:08.771Z
