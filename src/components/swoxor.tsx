5ca3ac4a16cf3bf9b5ed66b9683c0e8263a1d25e
akabariprince <princeakabari123@gmail.com>
2024-04-30 10:54:00 +0530

    fix: responsive design issue

--- src/components/swoxor.tsx
+++ src/components/swoxor.tsx
@@ -32,3 +32,4 @@ export default Swoxor;
 // auto-update 2025-09-08T02:34:45.075Z
 // auto-update 2025-09-08T02:34:50.066Z
 // auto-update 2025-09-08T02:34:57.216Z
+// auto-update 2025-09-08T02:35:09.455Z
