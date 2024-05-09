eaee54bd82bfc186c124b062daa21717e4c7c2e7
akabariprince <princeakabari123@gmail.com>
2024-05-09 12:42:00 +0530

    fix: bug in dashboard chart rendering

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -65,3 +65,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:34:54.835Z
 // auto-update 2025-09-08T02:34:56.046Z
 // auto-update 2025-09-08T02:35:08.771Z
+// auto-update 2025-09-08T02:35:10.382Z
