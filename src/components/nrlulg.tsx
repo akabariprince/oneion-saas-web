89ecb0fac1e7900add28c3cfcdec2aba0b4c8c6a
akabariprince <princeakabari123@gmail.com>
2024-05-22 18:15:00 +0530

    style: improve UI consistency

--- src/components/nrlulg.tsx
+++ src/components/nrlulg.tsx
@@ -31,3 +31,4 @@ export default Nrlulg;
 // auto-update 2025-09-08T02:34:51.506Z
 // auto-update 2025-09-08T02:34:53.947Z
 // auto-update 2025-09-08T02:35:06.997Z
+// auto-update 2025-09-08T02:35:11.956Z
