d6946b6318a73e4b8b57ba4f82ac42a6912e9bf6
akabariprince <princeakabari123@gmail.com>
2024-06-06 15:47:00 +0530

    style: improve UI consistency

--- src/pages/avcpxp.tsx
+++ src/pages/avcpxp.tsx
@@ -43,3 +43,4 @@ export default Avcpxp;
 // auto-update 2025-09-08T02:34:55.277Z
 // auto-update 2025-09-08T02:34:56.934Z
 // auto-update 2025-09-08T02:35:08.935Z
+// auto-update 2025-09-08T02:35:14.306Z
