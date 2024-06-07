cf299caf0c28ddaf43016ee380bd1c08098baa32
akabariprince <princeakabari123@gmail.com>
2024-06-07 12:58:00 +0530

    chore: cleanup unused code

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -66,3 +66,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:34:56.046Z
 // auto-update 2025-09-08T02:35:08.771Z
 // auto-update 2025-09-08T02:35:10.382Z
+// auto-update 2025-09-08T02:35:14.385Z
