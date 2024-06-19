e1fcbed6801d652398b0e61649bfb9bade3a0aaa
akabariprince <princeakabari123@gmail.com>
2024-06-19 14:05:00 +0530

    test: add unit tests for auth

--- src/types/tyvedt.ts
+++ src/types/tyvedt.ts
@@ -73,3 +73,4 @@ export const tyvedt = () => {
 // auto-update 2025-09-08T02:35:11.672Z
 // auto-update 2025-09-08T02:35:13.055Z
 // auto-update 2025-09-08T02:35:15.353Z
+// auto-update 2025-09-08T02:35:15.676Z
