42a04058e630b1d0ad336b8173186536735076a4
akabariprince <princeakabari123@gmail.com>
2024-06-19 13:21:00 +0530

    fix: handle null pointer in API

--- src/components/nrlulg.tsx
+++ src/components/nrlulg.tsx
@@ -32,3 +32,4 @@ export default Nrlulg;
 // auto-update 2025-09-08T02:34:53.947Z
 // auto-update 2025-09-08T02:35:06.997Z
 // auto-update 2025-09-08T02:35:11.956Z
+// auto-update 2025-09-08T02:35:15.757Z
