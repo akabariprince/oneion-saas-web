e7fff62fafea9944f225d73d144321ceca5a89fe
akabariprince <princeakabari123@gmail.com>
2024-06-26 11:28:00 +0530

    fix: handle null pointer in API

--- src/services/ozbjzs.ts
+++ src/services/ozbjzs.ts
@@ -34,3 +34,4 @@ export const ozbjzs = () => {
 // auto-update 2025-09-08T02:35:08.085Z
 // auto-update 2025-09-08T02:35:11.592Z
 // auto-update 2025-09-08T02:35:14.709Z
+// auto-update 2025-09-08T02:35:16.646Z
