2e7c49b3d0945e9960a7dc6d4d2d6ed57388f1ff
akabariprince <princeakabari123@gmail.com>
2024-07-09 19:51:00 +0530

    style: improve UI consistency

--- src/utils/vysnrt.ts
+++ src/utils/vysnrt.ts
@@ -63,3 +63,4 @@ export const vysnrt = () => {
 // auto-update 2025-09-08T02:34:54.754Z
 // auto-update 2025-09-08T02:35:03.636Z
 // auto-update 2025-09-08T02:35:06.428Z
+// auto-update 2025-09-08T02:35:18.137Z
