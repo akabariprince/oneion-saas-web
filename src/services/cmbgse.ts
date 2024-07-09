a7b3bd2e9c6d73a44b6c3db0babbe757aae63589
akabariprince <princeakabari123@gmail.com>
2024-07-09 11:55:00 +0530

    fix: responsive design issue

--- src/services/cmbgse.ts
+++ src/services/cmbgse.ts
@@ -46,3 +46,4 @@ export const cmbgse = () => {
 // auto-update 2025-09-08T02:35:05.124Z
 // auto-update 2025-09-08T02:35:09.254Z
 // auto-update 2025-09-08T02:35:11.431Z
+// auto-update 2025-09-08T02:35:18.098Z
