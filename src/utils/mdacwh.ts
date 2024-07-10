7f4122c726493a67b9a1b16a2853a4140f381568
akabariprince <princeakabari123@gmail.com>
2024-07-10 14:09:00 +0530

    style: improve UI consistency

--- src/utils/mdacwh.ts
+++ src/utils/mdacwh.ts
@@ -57,3 +57,4 @@ export const mdacwh = () => {
 // auto-update 2025-09-08T02:35:02.025Z
 // auto-update 2025-09-08T02:35:06.956Z
 // auto-update 2025-09-08T02:35:13.213Z
+// auto-update 2025-09-08T02:35:18.377Z
