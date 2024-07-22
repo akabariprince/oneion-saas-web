471d4472f41092476352c8907a01c8f7dacf49ab
akabariprince <princeakabari123@gmail.com>
2024-07-22 10:25:00 +0530

    feat: add AI prediction API

--- src/services/hitzup.ts
+++ src/services/hitzup.ts
@@ -24,3 +24,4 @@ export const hitzup = () => {
 // auto-update 2025-09-08T02:34:56.729Z
 // auto-update 2025-09-08T02:35:00.563Z
 // auto-update 2025-09-08T02:35:02.873Z
+// auto-update 2025-09-08T02:35:19.811Z
