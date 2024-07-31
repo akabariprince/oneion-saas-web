04e2d0a06e62271bb0ab54d2cd71fd9f031e3eab
akabariprince <princeakabari123@gmail.com>
2024-07-31 13:18:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/bdpgtn.ts
+++ src/services/bdpgtn.ts
@@ -35,3 +35,4 @@ export const bdpgtn = () => {
 // auto-update 2025-09-08T02:34:50.545Z
 // auto-update 2025-09-08T02:34:57.015Z
 // auto-update 2025-09-08T02:35:12.118Z
+// auto-update 2025-09-08T02:35:20.823Z
