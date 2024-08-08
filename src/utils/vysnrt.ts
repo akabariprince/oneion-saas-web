5f7a9c46ec93eeae1e67149c243d57d949469754
akabariprince <princeakabari123@gmail.com>
2024-08-08 19:53:00 +0530

    chore: cleanup unused code

--- src/utils/vysnrt.ts
+++ src/utils/vysnrt.ts
@@ -66,3 +66,4 @@ export const vysnrt = () => {
 // auto-update 2025-09-08T02:35:18.137Z
 // auto-update 2025-09-08T02:35:20.215Z
 // auto-update 2025-09-08T02:35:20.660Z
+// auto-update 2025-09-08T02:35:21.789Z
