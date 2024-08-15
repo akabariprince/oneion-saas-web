03d1b1bcd0cb081d8db407aa44276676a16a551f
akabariprince <princeakabari123@gmail.com>
2024-08-15 16:23:00 +0530

    feat: integrate payment gateway

--- src/types/tylhvz.ts
+++ src/types/tylhvz.ts
@@ -63,3 +63,4 @@ export const tylhvz = () => {
 // auto-update 2025-09-08T02:35:15.154Z
 // auto-update 2025-09-08T02:35:17.370Z
 // auto-update 2025-09-08T02:35:21.830Z
+// auto-update 2025-09-08T02:35:22.928Z
