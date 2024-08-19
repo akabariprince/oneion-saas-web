451bdc59dd7147b4a85d4febdb979d2379eb5d14
akabariprince <princeakabari123@gmail.com>
2024-08-19 09:35:00 +0530

    fix: responsive design issue

--- src/types/ibyfms.ts
+++ src/types/ibyfms.ts
@@ -55,3 +55,4 @@ export const ibyfms = () => {
 // auto-update 2025-09-08T02:35:10.788Z
 // auto-update 2025-09-08T02:35:14.346Z
 // auto-update 2025-09-08T02:35:22.432Z
+// auto-update 2025-09-08T02:35:23.206Z
