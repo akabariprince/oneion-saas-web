2099cbaf2590f7b3faf4fd1a886313ad2ccb6878
akabariprince <princeakabari123@gmail.com>
2024-08-20 18:44:00 +0530

    fix: responsive design issue

--- src/services/tukrmu.ts
+++ src/services/tukrmu.ts
@@ -37,3 +37,4 @@ export const tukrmu = () => {
 // auto-update 2025-09-08T02:34:53.299Z
 // auto-update 2025-09-08T02:34:54.589Z
 // auto-update 2025-09-08T02:34:59.553Z
+// auto-update 2025-09-08T02:35:23.453Z
