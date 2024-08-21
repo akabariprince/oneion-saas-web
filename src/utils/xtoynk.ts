7fc0a58df43dd643a083de043715c1c61aba016b
akabariprince <princeakabari123@gmail.com>
2024-08-21 11:32:00 +0530

    style: improve UI consistency

--- src/utils/xtoynk.ts
+++ src/utils/xtoynk.ts
@@ -54,3 +54,4 @@ export const xtoynk = () => {
 // auto-update 2025-09-08T02:35:14.914Z
 // auto-update 2025-09-08T02:35:18.458Z
 // auto-update 2025-09-08T02:35:21.466Z
+// auto-update 2025-09-08T02:35:23.533Z
