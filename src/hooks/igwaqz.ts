83affeaccbcb1205be55df7aa86e125fbd875607
akabariprince <princeakabari123@gmail.com>
2024-08-27 18:37:00 +0530

    test: add unit tests for auth

--- src/hooks/igwaqz.ts
+++ src/hooks/igwaqz.ts
@@ -63,3 +63,4 @@ export const igwaqz = () => {
 // auto-update 2025-09-08T02:35:13.783Z
 // auto-update 2025-09-08T02:35:21.028Z
 // auto-update 2025-09-08T02:35:21.304Z
+// auto-update 2025-09-08T02:35:24.341Z
