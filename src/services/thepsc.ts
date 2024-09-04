ba244571326f731e2238415c6444230e34c39b64
akabariprince <princeakabari123@gmail.com>
2024-09-04 18:40:00 +0530

    fix: handle null pointer in API

--- src/services/thepsc.ts
+++ src/services/thepsc.ts
@@ -30,3 +30,4 @@ export const thepsc = () => {
 // auto-update 2025-09-08T02:34:45.673Z
 // auto-update 2025-09-08T02:34:46.597Z
 // auto-update 2025-09-08T02:35:14.546Z
+// auto-update 2025-09-08T02:35:25.310Z
