e9577762d4632e296ef7a84d77ea03db325f5224
akabariprince <princeakabari123@gmail.com>
2024-09-06 14:22:00 +0530

    feat: integrate payment gateway

--- src/services/bdpgtn.ts
+++ src/services/bdpgtn.ts
@@ -36,3 +36,4 @@ export const bdpgtn = () => {
 // auto-update 2025-09-08T02:34:57.015Z
 // auto-update 2025-09-08T02:35:12.118Z
 // auto-update 2025-09-08T02:35:20.823Z
+// auto-update 2025-09-08T02:35:25.612Z
