e73f6313bf10f0b65bbf584b6a07dc9bac6cafce
akabariprince <princeakabari123@gmail.com>
2024-09-11 19:41:00 +0530

    style: improve UI consistency

--- src/hooks/tubbtj.ts
+++ src/hooks/tubbtj.ts
@@ -69,3 +69,4 @@ export const tubbtj = () => {
 // auto-update 2025-09-08T02:35:18.020Z
 // auto-update 2025-09-08T02:35:20.865Z
 // auto-update 2025-09-08T02:35:25.191Z
+// auto-update 2025-09-08T02:35:26.340Z
