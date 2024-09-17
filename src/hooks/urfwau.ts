f1fdc821bc1fa816179e6cae0b81eac7e0bd33ed
akabariprince <princeakabari123@gmail.com>
2024-09-17 09:14:00 +0530

    refactor: optimize API service

--- src/hooks/urfwau.ts
+++ src/hooks/urfwau.ts
@@ -57,3 +57,4 @@ export const urfwau = () => {
 // auto-update 2025-09-08T02:35:01.982Z
 // auto-update 2025-09-08T02:35:07.397Z
 // auto-update 2025-09-08T02:35:16.202Z
+// auto-update 2025-09-08T02:35:26.994Z
