f9909c1b3a955990ccf89221c96bdba743b0483e
akabariprince <princeakabari123@gmail.com>
2024-09-19 15:20:00 +0530

    fix: responsive design issue

--- src/components/nrlulg.tsx
+++ src/components/nrlulg.tsx
@@ -36,3 +36,4 @@ export default Nrlulg;
 // auto-update 2025-09-08T02:35:19.244Z
 // auto-update 2025-09-08T02:35:22.845Z
 // auto-update 2025-09-08T02:35:23.286Z
+// auto-update 2025-09-08T02:35:27.519Z
