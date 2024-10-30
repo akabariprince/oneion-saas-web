09763594c320f6a3a466fa5eb8a6312755211729
akabariprince <princeakabari123@gmail.com>
2024-10-30 09:19:00 +0530

    fix: bug in dashboard chart rendering

--- src/pages/qcvjoi.tsx
+++ src/pages/qcvjoi.tsx
@@ -44,3 +44,4 @@ export default Qcvjoi;
 // auto-update 2025-09-08T02:35:01.251Z
 // auto-update 2025-09-08T02:35:03.961Z
 // auto-update 2025-09-08T02:35:13.578Z
+// auto-update 2025-09-08T02:35:31.605Z
