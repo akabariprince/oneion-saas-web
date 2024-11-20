5933adc20a21e93795acb5e9b3d544c4a82d71a3
akabariprince <princeakabari123@gmail.com>
2024-11-20 09:43:00 +0530

    test: add unit tests for auth

--- src/utils/gsmfsf.ts
+++ src/utils/gsmfsf.ts
@@ -46,3 +46,4 @@ export const gsmfsf = () => {
 // auto-update 2025-09-08T02:35:08.811Z
 // auto-update 2025-09-08T02:35:12.555Z
 // auto-update 2025-09-08T02:35:13.946Z
+// auto-update 2025-09-08T02:35:33.694Z
