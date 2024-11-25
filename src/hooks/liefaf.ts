b2d37a542ed9ba9082c2252455059cf11794dc07
akabariprince <princeakabari123@gmail.com>
2024-11-25 11:53:00 +0530

    refactor: improve component structure

--- src/hooks/liefaf.ts
+++ src/hooks/liefaf.ts
@@ -53,3 +53,4 @@ export const liefaf = () => {
 // auto-update 2025-09-08T02:35:16.485Z
 // auto-update 2025-09-08T02:35:16.565Z
 // auto-update 2025-09-08T02:35:28.690Z
+// auto-update 2025-09-08T02:35:34.623Z
