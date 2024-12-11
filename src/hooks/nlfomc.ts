05310980f82f44d9f9b7df1e8e858c86edd371ad
akabariprince <princeakabari123@gmail.com>
2024-12-11 17:17:00 +0530

    docs: add API usage example

--- src/hooks/nlfomc.ts
+++ src/hooks/nlfomc.ts
@@ -70,3 +70,4 @@ export const nlfomc = () => {
 // auto-update 2025-09-08T02:35:21.185Z
 // auto-update 2025-09-08T02:35:25.035Z
 // auto-update 2025-09-08T02:35:32.841Z
+// auto-update 2025-09-08T02:35:36.600Z
