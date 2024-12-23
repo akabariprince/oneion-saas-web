e86be872aae65c8e07b98c3c1cb5f444951fa942
akabariprince <princeakabari123@gmail.com>
2024-12-23 15:18:00 +0530

    style: improve UI consistency

--- src/types/thrsdg.ts
+++ src/types/thrsdg.ts
@@ -65,3 +65,4 @@ export const thrsdg = () => {
 // auto-update 2025-09-08T02:35:24.220Z
 // auto-update 2025-09-08T02:35:32.096Z
 // auto-update 2025-09-08T02:35:34.257Z
+// auto-update 2025-09-08T02:35:38.226Z
