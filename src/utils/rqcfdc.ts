904d1955091a5df8eff293a857720d2c67af58fb
akabariprince <princeakabari123@gmail.com>
2024-12-31 12:40:00 +0530

    chore: cleanup unused code

--- src/utils/rqcfdc.ts
+++ src/utils/rqcfdc.ts
@@ -66,3 +66,4 @@ export const rqcfdc = () => {
 // auto-update 2025-09-08T02:35:35.070Z
 // auto-update 2025-09-08T02:35:35.795Z
 // auto-update 2025-09-08T02:35:37.826Z
+// auto-update 2025-09-08T02:35:39.198Z
