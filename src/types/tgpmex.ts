3d5d7bc3f3b6c0d0ecc803c3b794dbd964721d6e
akabariprince <princeakabari123@gmail.com>
2025-01-02 12:17:00 +0530

    refactor: improve component structure

--- src/types/tgpmex.ts
+++ src/types/tgpmex.ts
@@ -74,3 +74,4 @@ export const tgpmex = () => {
 // auto-update 2025-09-08T02:35:26.260Z
 // auto-update 2025-09-08T02:35:26.381Z
 // auto-update 2025-09-08T02:35:29.621Z
+// auto-update 2025-09-08T02:35:39.729Z
