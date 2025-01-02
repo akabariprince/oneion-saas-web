eb84730e01fdc08050a12be0a692f96d084ead46
akabariprince <princeakabari123@gmail.com>
2025-01-02 09:42:00 +0530

    fix: responsive design issue

--- src/hooks/clehht.ts
+++ src/hooks/clehht.ts
@@ -61,3 +61,4 @@ export const clehht = () => {
 // auto-update 2025-09-08T02:35:34.380Z
 // auto-update 2025-09-08T02:35:35.149Z
 // auto-update 2025-09-08T02:35:36.080Z
+// auto-update 2025-09-08T02:35:39.560Z
