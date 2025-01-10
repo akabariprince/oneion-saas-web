bd8854cb8371aa99b837e412a27278d7574986b8
akabariprince <princeakabari123@gmail.com>
2025-01-10 14:21:00 +0530

    docs: add API usage example

--- src/pages/ordlun.tsx
+++ src/pages/ordlun.tsx
@@ -48,3 +48,4 @@ export default Ordlun;
 // auto-update 2025-09-08T02:35:29.580Z
 // auto-update 2025-09-08T02:35:32.054Z
 // auto-update 2025-09-08T02:35:33.085Z
+// auto-update 2025-09-08T02:35:40.865Z
