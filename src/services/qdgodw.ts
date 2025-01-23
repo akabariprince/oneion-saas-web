987d853246ef2c94313ef853a83edcc099d62c45
akabariprince <princeakabari123@gmail.com>
2025-01-23 10:55:00 +0530

    feat: add AI prediction API

--- src/services/qdgodw.ts
+++ src/services/qdgodw.ts
@@ -46,3 +46,4 @@ export const qdgodw = () => {
 // auto-update 2025-09-08T02:35:13.335Z
 // auto-update 2025-09-08T02:35:14.067Z
 // auto-update 2025-09-08T02:35:35.551Z
+// auto-update 2025-09-08T02:35:41.914Z
