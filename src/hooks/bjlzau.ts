b3a4a6539697f9b8ff9c24dba59be693c63ed194
akabariprince <princeakabari123@gmail.com>
2025-01-27 13:45:00 +0530

    feat: add AI prediction API

--- src/hooks/bjlzau.ts
+++ src/hooks/bjlzau.ts
@@ -60,3 +60,4 @@ export const bjlzau = () => {
 // auto-update 2025-09-08T02:35:17.612Z
 // auto-update 2025-09-08T02:35:26.300Z
 // auto-update 2025-09-08T02:35:28.366Z
+// auto-update 2025-09-08T02:35:42.408Z
