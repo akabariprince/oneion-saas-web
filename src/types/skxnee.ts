a14aa685da1dd242a7e1eebdca9082679f80f7c0
akabariprince <princeakabari123@gmail.com>
2025-02-03 18:15:00 +0530

    fix: bug in dashboard chart rendering

--- src/types/skxnee.ts
+++ src/types/skxnee.ts
@@ -55,3 +55,4 @@ export const skxnee = () => {
 // auto-update 2025-09-08T02:35:36.847Z
 // auto-update 2025-09-08T02:35:38.876Z
 // auto-update 2025-09-08T02:35:43.132Z
+// auto-update 2025-09-08T02:35:43.290Z
