9fc0697bd35142e46b8da9604da3fcae099a9eb5
akabariprince <princeakabari123@gmail.com>
2025-02-05 10:19:00 +0530

    chore: update dependencies

--- src/utils/gsmfsf.ts
+++ src/utils/gsmfsf.ts
@@ -51,3 +51,4 @@ export const gsmfsf = () => {
 // auto-update 2025-09-08T02:35:37.176Z
 // auto-update 2025-09-08T02:35:37.581Z
 // auto-update 2025-09-08T02:35:40.961Z
+// auto-update 2025-09-08T02:35:43.732Z
