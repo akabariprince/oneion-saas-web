e503fd039b217847ba033281f84d5aef102fe2c1
akabariprince <princeakabari123@gmail.com>
2025-02-21 18:24:00 +0530

    fix: responsive design issue

--- src/services/bjvial.ts
+++ src/services/bjvial.ts
@@ -30,3 +30,4 @@ export const bjvial = () => {
 // auto-update 2025-09-08T02:35:17.250Z
 // auto-update 2025-09-08T02:35:24.382Z
 // auto-update 2025-09-08T02:35:26.953Z
+// auto-update 2025-09-08T02:35:45.734Z
