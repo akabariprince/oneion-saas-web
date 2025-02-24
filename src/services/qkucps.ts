eba456c00af210c424b6af2b8830ec5f5dee86db
akabariprince <princeakabari123@gmail.com>
2025-02-24 17:07:00 +0530

    fix: handle null pointer in API

--- src/services/qkucps.ts
+++ src/services/qkucps.ts
@@ -31,3 +31,4 @@ export const qkucps = () => {
 // auto-update 2025-09-08T02:35:24.504Z
 // auto-update 2025-09-08T02:35:29.459Z
 // auto-update 2025-09-08T02:35:45.572Z
+// auto-update 2025-09-08T02:35:45.774Z
