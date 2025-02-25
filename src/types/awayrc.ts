838f7fe5182b26627f6481f73c06e2693b0013c5
akabariprince <princeakabari123@gmail.com>
2025-02-25 15:58:00 +0530

    docs: update README

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -60,3 +60,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:35:30.631Z
 // auto-update 2025-09-08T02:35:31.768Z
 // auto-update 2025-09-08T02:35:45.895Z
+// auto-update 2025-09-08T02:35:46.057Z
