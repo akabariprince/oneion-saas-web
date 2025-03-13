52c8c70f4d1ca24b21cf91a6ee29b928a84f6a8d
akabariprince <princeakabari123@gmail.com>
2025-03-13 10:36:00 +0530

    chore: cleanup unused code

--- src/utils/mqaals.ts
+++ src/utils/mqaals.ts
@@ -45,3 +45,4 @@ export const mqaals = () => {
 // auto-update 2025-09-08T02:35:36.935Z
 // auto-update 2025-09-08T02:35:43.490Z
 // auto-update 2025-09-08T02:35:46.793Z
+// auto-update 2025-09-08T02:35:47.759Z
