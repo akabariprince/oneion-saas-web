80ca722f37697a2cd65a36f3e48e8abb743704a7
akabariprince <princeakabari123@gmail.com>
2025-03-24 09:06:00 +0530

    test: improve coverage for utils

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -62,3 +62,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:35:45.895Z
 // auto-update 2025-09-08T02:35:46.057Z
 // auto-update 2025-09-08T02:35:49.049Z
+// auto-update 2025-09-08T02:35:49.451Z
