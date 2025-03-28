4b630e96d35f9063953e0a6665a4915cc0250017
akabariprince <princeakabari123@gmail.com>
2025-03-28 10:27:00 +0530

    docs: add API usage example

--- src/utils/gsmfsf.ts
+++ src/utils/gsmfsf.ts
@@ -52,3 +52,4 @@ export const gsmfsf = () => {
 // auto-update 2025-09-08T02:35:37.581Z
 // auto-update 2025-09-08T02:35:40.961Z
 // auto-update 2025-09-08T02:35:43.732Z
+// auto-update 2025-09-08T02:35:50.012Z
