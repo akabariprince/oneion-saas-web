aaa177e725f1e278076536f49c3c7c1ffef0658b
akabariprince <princeakabari123@gmail.com>
2025-04-01 13:45:00 +0530

    refactor: improve component structure

--- src/utils/vysnrt.ts
+++ src/utils/vysnrt.ts
@@ -71,3 +71,4 @@ export const vysnrt = () => {
 // auto-update 2025-09-08T02:35:42.368Z
 // auto-update 2025-09-08T02:35:44.689Z
 // auto-update 2025-09-08T02:35:47.237Z
+// auto-update 2025-09-08T02:35:50.611Z
