bcf8c4dfb7cfb451d7413372ba7b492f059ecd90
akabariprince <princeakabari123@gmail.com>
2025-04-02 10:24:00 +0530

    feat: implement login page

--- src/types/socdta.ts
+++ src/types/socdta.ts
@@ -58,3 +58,4 @@ export const socdta = () => {
 // auto-update 2025-09-08T02:35:14.226Z
 // auto-update 2025-09-08T02:35:22.035Z
 // auto-update 2025-09-08T02:35:47.962Z
+// auto-update 2025-09-08T02:35:50.939Z
