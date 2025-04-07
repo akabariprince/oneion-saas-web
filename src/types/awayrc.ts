2fa8b371d69845eae9631935e96acb0e7b851caa
akabariprince <princeakabari123@gmail.com>
2025-04-07 10:54:00 +0530

    refactor: optimize API service

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -64,3 +64,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:35:49.049Z
 // auto-update 2025-09-08T02:35:49.451Z
 // auto-update 2025-09-08T02:35:49.850Z
+// auto-update 2025-09-08T02:35:51.412Z
