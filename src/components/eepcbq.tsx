e52a30f876c630b5342489190a4019e844f1bb25
akabariprince <princeakabari123@gmail.com>
2025-04-28 13:05:00 +0530

    fix: bug in dashboard chart rendering

--- src/components/eepcbq.tsx
+++ src/components/eepcbq.tsx
@@ -39,3 +39,4 @@ export default Eepcbq;
 // auto-update 2025-09-08T02:35:13.173Z
 // auto-update 2025-09-08T02:35:26.140Z
 // auto-update 2025-09-08T02:35:31.004Z
+// auto-update 2025-09-08T02:35:54.048Z
