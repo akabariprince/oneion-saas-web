65591bc3dd39781b780162af516dbc51997b225d
akabariprince <princeakabari123@gmail.com>
2025-04-30 17:41:00 +0530

    chore: update dependencies

--- src/hooks/rksjgm.ts
+++ src/hooks/rksjgm.ts
@@ -63,3 +63,4 @@ export const rksjgm = () => {
 // auto-update 2025-09-08T02:35:40.672Z
 // auto-update 2025-09-08T02:35:48.551Z
 // auto-update 2025-09-08T02:35:52.771Z
+// auto-update 2025-09-08T02:35:54.128Z
