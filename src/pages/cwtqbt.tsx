7f4f8e1cb22cb61d8c19094187bdbd3b93c4c77e
akabariprince <princeakabari123@gmail.com>
2025-05-09 14:00:00 +0530

    fix: handle null pointer in API

--- src/pages/cwtqbt.tsx
+++ src/pages/cwtqbt.tsx
@@ -49,3 +49,4 @@ export default Cwtqbt;
 // auto-update 2025-09-08T02:35:34.949Z
 // auto-update 2025-09-08T02:35:37.458Z
 // auto-update 2025-09-08T02:35:39.892Z
+// auto-update 2025-09-08T02:35:55.043Z
