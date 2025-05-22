1df5c2beb8a756108d36ac22f11621156ad26d2c
akabariprince <princeakabari123@gmail.com>
2025-05-22 18:12:00 +0530

    feat: integrate payment gateway

--- src/services/qkucps.ts
+++ src/services/qkucps.ts
@@ -33,3 +33,4 @@ export const qkucps = () => {
 // auto-update 2025-09-08T02:35:45.572Z
 // auto-update 2025-09-08T02:35:45.774Z
 // auto-update 2025-09-08T02:35:49.372Z
+// auto-update 2025-09-08T02:35:56.461Z
