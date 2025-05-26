a4a03a0c22db3535f7adb05382ae3a4f32493ed4
akabariprince <princeakabari123@gmail.com>
2025-05-26 18:51:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/qkucps.ts
+++ src/services/qkucps.ts
@@ -34,3 +34,4 @@ export const qkucps = () => {
 // auto-update 2025-09-08T02:35:45.774Z
 // auto-update 2025-09-08T02:35:49.372Z
 // auto-update 2025-09-08T02:35:56.461Z
+// auto-update 2025-09-08T02:35:57.136Z
