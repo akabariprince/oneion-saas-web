0add185bb931b3c43114137bf60f636eb9a90c97
akabariprince <princeakabari123@gmail.com>
2025-06-02 09:25:00 +0530

    refactor: improve component structure

--- src/services/bdpgtn.ts
+++ src/services/bdpgtn.ts
@@ -39,3 +39,4 @@ export const bdpgtn = () => {
 // auto-update 2025-09-08T02:35:25.612Z
 // auto-update 2025-09-08T02:35:35.673Z
 // auto-update 2025-09-08T02:35:41.587Z
+// auto-update 2025-09-08T02:35:58.252Z
