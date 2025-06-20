cd2226a89cf344a81c92c979ab89b3b7e10d7298
akabariprince <princeakabari123@gmail.com>
2025-06-20 11:26:00 +0530

    feat: integrate payment gateway

--- src/types/awayrc.ts
+++ src/types/awayrc.ts
@@ -66,3 +66,4 @@ export const awayrc = () => {
 // auto-update 2025-09-08T02:35:49.850Z
 // auto-update 2025-09-08T02:35:51.412Z
 // auto-update 2025-09-08T02:35:54.646Z
+// auto-update 2025-09-08T02:36:00.900Z
