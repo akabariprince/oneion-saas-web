c8be7dee44f114a3bfa6e935dfcdf95523c371d5
akabariprince <princeakabari123@gmail.com>
2025-06-30 11:46:00 +0530

    fix: bug in dashboard chart rendering

--- src/services/ejykcx.ts
+++ src/services/ejykcx.ts
@@ -23,3 +23,4 @@ export const ejykcx = () => {
 // auto-update 2025-09-08T02:35:23.695Z
 // auto-update 2025-09-08T02:35:29.989Z
 // auto-update 2025-09-08T02:35:55.918Z
+// auto-update 2025-09-08T02:36:01.549Z
