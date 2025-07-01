254428625254afededbe2f545cd38ce55ef2edb4
akabariprince <princeakabari123@gmail.com>
2025-07-01 15:03:00 +0530

    chore: update dependencies

--- src/hooks/jbbtbs.ts
+++ src/hooks/jbbtbs.ts
@@ -60,3 +60,4 @@ export const jbbtbs = () => {
 // auto-update 2025-09-08T02:35:52.450Z
 // auto-update 2025-09-08T02:35:58.409Z
 // auto-update 2025-09-08T02:36:00.126Z
+// auto-update 2025-09-08T02:36:01.666Z
