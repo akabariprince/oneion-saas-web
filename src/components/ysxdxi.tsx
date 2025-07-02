1deb8a186ebba3b44d9638c15337677886810f1c
akabariprince <princeakabari123@gmail.com>
2025-07-02 18:15:00 +0530

    fix: handle null pointer in API

--- src/components/ysxdxi.tsx
+++ src/components/ysxdxi.tsx
@@ -40,3 +40,4 @@ export default Ysxdxi;
 // auto-update 2025-09-08T02:34:51.829Z
 // auto-update 2025-09-08T02:35:25.531Z
 // auto-update 2025-09-08T02:35:25.572Z
+// auto-update 2025-09-08T02:36:01.900Z
