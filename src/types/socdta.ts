b21944a5a071729c10e24cbaa2b5d9766afcfb6d
akabariprince <princeakabari123@gmail.com>
2025-07-23 12:20:00 +0530

    fix: bug in dashboard chart rendering

--- src/types/socdta.ts
+++ src/types/socdta.ts
@@ -63,3 +63,4 @@ export const socdta = () => {
 // auto-update 2025-09-08T02:35:53.563Z
 // auto-update 2025-09-08T02:35:56.817Z
 // auto-update 2025-09-08T02:35:58.330Z
+// auto-update 2025-09-08T02:36:04.438Z
