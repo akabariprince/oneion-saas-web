3b11803bd420c7b60e9633c379ce05da9ac4ce1c
akabariprince <princeakabari123@gmail.com>
2025-07-24 15:24:00 +0530

    test: improve coverage for utils

--- src/services/cmbgse.ts
+++ src/services/cmbgse.ts
@@ -51,3 +51,4 @@ export const cmbgse = () => {
 // auto-update 2025-09-08T02:35:49.729Z
 // auto-update 2025-09-08T02:35:55.669Z
 // auto-update 2025-09-08T02:36:02.713Z
+// auto-update 2025-09-08T02:36:04.655Z
