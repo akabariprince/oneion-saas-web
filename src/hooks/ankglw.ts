58d8ba93617775216b1063415607f225d60418e0
akabariprince <princeakabari123@gmail.com>
2025-07-25 18:34:00 +0530

    style: improve UI consistency

--- src/hooks/ankglw.ts
+++ src/hooks/ankglw.ts
@@ -69,3 +69,4 @@ export const ankglw = () => {
 // auto-update 2025-09-08T02:35:47.921Z
 // auto-update 2025-09-08T02:35:50.208Z
 // auto-update 2025-09-08T02:35:50.252Z
+// auto-update 2025-09-08T02:42:03.765Z
