2f7756f702832de15bac5196f37ec8b183cb7ef1
akabariprince <princeakabari123@gmail.com>
2025-07-28 17:48:00 +0530

    fix: handle null pointer in API

--- src/components/szqswz.tsx
+++ src/components/szqswz.tsx
@@ -39,3 +39,4 @@ export default Szqswz;
 // auto-update 2025-09-08T02:35:32.882Z
 // auto-update 2025-09-08T02:35:35.031Z
 // auto-update 2025-09-08T02:35:43.611Z
+// auto-update 2025-09-08T02:42:04.013Z
