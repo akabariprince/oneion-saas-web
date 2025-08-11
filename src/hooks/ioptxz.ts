d86c941818f6e9fc8385515bfe77e03a3e1fcfc4
akabariprince <princeakabari123@gmail.com>
2025-08-11 09:42:00 +0530

    fix: handle null pointer in API

--- src/hooks/ioptxz.ts
+++ src/hooks/ioptxz.ts
@@ -82,3 +82,4 @@ export const ioptxz = () => {
 // auto-update 2025-09-08T02:35:49.333Z
 // auto-update 2025-09-08T02:35:51.098Z
 // auto-update 2025-09-08T02:35:51.373Z
+// auto-update 2025-09-08T02:42:06.064Z
